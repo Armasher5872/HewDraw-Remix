c97f5d45faca3092679b776c4ce05d446e33fc0b
SuddyN <suddy121800@hotmail.com>
2024-12-03 00:05:58 -0500

    terry DAir nerf

--- fighters/dolly/src/acmd/aerials.rs
+++ fighters/dolly/src/acmd/aerials.rs
@@ -116,6 +116,10 @@ unsafe extern "C" fn game_attackairhi(agent: &mut L2CAgentBase) {
 unsafe extern "C" fn game_attackairlw(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
+    frame(lua_state, 3.0);
+    FT_MOTION_RATE_RANGE(agent, 3.0, 7.0, 7.0);
+    frame(lua_state, 7.0);
+    FT_MOTION_RATE(agent, 1.0);
     frame(lua_state, 12.0);
     if is_excute(agent) {
         agent.on_flag(*FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
