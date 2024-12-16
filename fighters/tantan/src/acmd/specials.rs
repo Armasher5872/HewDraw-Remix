7704eeeb8e17b4537c6eb8af10e86da83e936507
Askew <sqk9gw@gmail.com>
2024-12-16 04:42:49 -0500

    Mim

--- fighters/tantan/src/acmd/specials.rs
+++ fighters/tantan/src/acmd/specials.rs
@@ -199,6 +199,111 @@ unsafe extern "C" fn expression_specialairnend(agent: &mut L2CAgentBase) {
     }
 }
 
+unsafe extern "C" fn effect_specialhistart(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    frame(lua_state, 1.0);
+    if is_excute(agent) {
+        EFFECT(agent, Hash40::new("sys_smash_flash_s"), Hash40::new("top"), -3.5, 13.5, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+    }
+    frame(lua_state, 3.0);
+    if is_excute(agent) {
+        if agent.is_situation(*SITUATION_KIND_GROUND) {
+            LANDING_EFFECT(agent, Hash40::new("sys_shield_smoke"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 0.9, 0, 0, 0, 0, 0, 0, false);
+        }
+    }
+}
+
+unsafe extern "C" fn effect_specialhilong(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if VarModule::is_flag(boma.object(), vars::tantan::instance::SPECIAL_HI_GROUND_START) {
+        if is_excute(agent) {
+            EFFECT_OFF_KIND(agent, Hash40::new("sys_shield_smoke"), false, true);
+            LANDING_EFFECT(agent, Hash40::new("tantan_jump_quake_l"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+            EFFECT(agent, Hash40::new("tantan_jump_wind_l"), Hash40::new("rot"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, true);
+        }
+        frame(lua_state, 4.0);
+        if is_excute(agent) {
+            EFFECT(agent, Hash40::new("sys_spin_wind_s"), Hash40::new("rot"), 0, 0.5, 0, 0, 0, 0, 1.5, 0, 10, 0, 5, 360, 5, true);
+            EFFECT_FOLLOW(agent, Hash40::new("tantan_jump_line_l"), Hash40::new("rot"), 0, 0, 0, 0, 0, 0, 1, false);
+            EffectModule::enable_sync_init_pos_last(boma);
+        }
+    }
+    else {
+        if is_excute(agent) {
+            EFFECT_OFF_KIND(agent, Hash40::new("sys_shield_smoke"), false, true);
+            LANDING_EFFECT(agent, Hash40::new("tantan_jump_quake_s"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 0.8, 0, 0, 0, 0, 0, 0, false);
+            EFFECT(agent, Hash40::new("tantan_jump_wind_s"), Hash40::new("rot"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, true);
+        }
+        frame(lua_state, 4.0);
+        if is_excute(agent) {
+            EFFECT_ALPHA(agent, Hash40::new("sys_spin_wind_s"), Hash40::new("top"), 0, 3, 0, 0, 0, 0, 1.2, 0, 2, 0, 5, 360, 5, true, 0.5);
+            EFFECT_FOLLOW(agent, Hash40::new("tantan_jump_line_s"), Hash40::new("rot"), 0, 0, 0, 0, 0, 0, 1, false);
+            EffectModule::enable_sync_init_pos_last(boma);
+        }
+    }
+}
+
+unsafe extern "C" fn sound_specialhilong(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if VarModule::is_flag(boma.object(), vars::tantan::instance::SPECIAL_HI_GROUND_START) {
+        frame(lua_state, 2.0);
+        if is_excute(agent) {
+            PLAY_SE(agent, Hash40::new("se_tantan_special_h05"));
+        }
+        frame(lua_state, 4.0);
+        if is_excute(agent) {
+            PLAY_SE(agent, Hash40::new("vc_tantan_special_h02"));
+        }
+    }
+    else {
+        frame(lua_state, 2.0);
+        if is_excute(agent) {
+            PLAY_SE(agent, Hash40::new("se_tantan_special_h01"));
+        }
+        frame(lua_state, 4.0);
+        if is_excute(agent) {
+            PLAY_SE(agent, Hash40::new("vc_tantan_special_h01"));
+        }
+    }
+}
+
+unsafe extern "C" fn expression_specialhilong(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if VarModule::is_flag(boma.object(), vars::tantan::instance::SPECIAL_HI_GROUND_START) {
+        if is_excute(agent) {
+            slope!(agent, *MA_MSC_CMD_SLOPE_SLOPE, *SLOPE_STATUS_LR);
+            ItemModule::set_have_item_visibility(boma, false, 0);
+        }
+        frame(lua_state, 2.0);
+        if is_excute(agent) {
+            slope!(agent, *MA_MSC_CMD_SLOPE_SLOPE_INTP, *SLOPE_STATUS_NONE, 2);
+        }
+        frame(lua_state, 4.0);
+        if is_excute(agent) {
+            ControlModule::set_rumble(boma, Hash40::new("rbkind_attackl"), 6, false, *BATTLE_OBJECT_ID_INVALID as u32);
+            QUAKE(agent, *CAMERA_QUAKE_KIND_M);
+        }
+    }
+    else {
+        if is_excute(agent) {
+            slope!(agent, *MA_MSC_CMD_SLOPE_SLOPE, *SLOPE_STATUS_LR);
+            ItemModule::set_have_item_visibility(boma, false, 0);
+        }
+        frame(lua_state, 2.0);
+        if is_excute(agent) {
+            slope!(agent, *MA_MSC_CMD_SLOPE_SLOPE_INTP, *SLOPE_STATUS_NONE, 2);
+        }
+        frame(lua_state, 4.0);
+        if is_excute(agent) {
+            ControlModule::set_rumble(boma, Hash40::new("rbkind_attackm"), 6, false, *BATTLE_OBJECT_ID_INVALID as u32);
+        }
+    }
+}
+
 unsafe extern "C" fn effect_specialairhi(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
@@ -272,6 +377,12 @@ pub fn install(agent: &mut Agent) {
     agent.acmd("effect_specialairnend", effect_specialairnend, Priority::Low);
     agent.acmd("sound_specialairnend", sound_specialairnend, Priority::Low);
     agent.acmd("expression_specialairnend", expression_specialairnend, Priority::Low);
+
+    agent.acmd("effect_specialhistart", effect_specialhistart, Priority::Low);
+
+    agent.acmd("effect_specialhilong", effect_specialhilong, Priority::Low);
+    agent.acmd("sound_specialhilong", sound_specialhilong, Priority::Low);
+    agent.acmd("expression_specialhilong", expression_specialhilong, Priority::Low);
 
     agent.acmd("effect_specialairhi", effect_specialairhi, Priority::Low);
     agent.acmd("effect_specialairhi2", effect_specialairhi, Priority::Low);
