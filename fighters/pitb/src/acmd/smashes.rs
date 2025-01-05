43bb3d371584e32aef5685ce67125c34f9f17e42
SuddyN <suddy121800@hotmail.com>
2025-01-04 21:19:09 -0500

    fix target vec

--- fighters/pitb/src/acmd/smashes.rs
+++ fighters/pitb/src/acmd/smashes.rs
@@ -47,8 +47,6 @@ unsafe extern "C" fn game_attackhi4(agent: &mut L2CAgentBase) {
         ATTACK(agent, 4, 0, Hash40::new("top"), 5.0, 105, 160, 0, 61, 5.5, 0.0, 24.0, 7.0, Some(0.0), Some(24.0), Some(-6.0), 1.25, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PALUTENA);
         ATTACK(agent, 5, 0, Hash40::new("top"), 5.0, 105, 160, 0, 61, 5.5, 0.0, 18.0, -5.5, Some(0.0), Some(18.0), Some(-4.5), 1.25, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PALUTENA);
         ATTACK(agent, 6, 0, Hash40::new("top"), 5.0, 105, 160, 0, 61, 5.5, 0.0, 18.0, -5.5, Some(0.0), Some(18.0), Some(-4.5), 1.25, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_A, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PALUTENA);
-        let hitVec = Vector2f { x: 0.0, y: 24.5 };
-        AttackModule::set_vec_target_pos(boma, 3, Hash40::new("top"), &hitVec, 10, false);
     }
     frame(lua_state, 7.0);
     FT_MOTION_RATE_RANGE(agent, 7.0, 9.0, 1.0);
