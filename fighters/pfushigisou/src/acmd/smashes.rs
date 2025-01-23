07eca75a3164d7535b1af32206528c802e093102
SuddyN <suddy121800@hotmail.com>
2025-01-22 23:58:33 -0500

    ivy usmash

--- fighters/pfushigisou/src/acmd/smashes.rs
+++ fighters/pfushigisou/src/acmd/smashes.rs
@@ -25,19 +25,13 @@ unsafe extern "C" fn game_attackhi4(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_FLAG_START_SMASH_HOLD);
     }
-    frame(lua_state, 11.0);
-    FT_MOTION_RATE(agent, 0.6);
-    frame(lua_state, 21.0);
-    FT_MOTION_RATE(agent, 1.0);
     frame(lua_state, 26.0);
     if is_excute(agent) {
-        HIT_NODE(agent, Hash40::new("flower"), *HIT_STATUS_XLU);
         ATTACK(agent, 0, 0, Hash40::new("top"), 17.0, 82, 72, 0, 73, 13.0, 0.0, 17.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_FIRE, *ATTACK_REGION_BOMB);
     }
-    frame(lua_state, 30.0);
+    wait(lua_state, 4.0);
     if is_excute(agent) {
         AttackModule::clear_all(boma);
-        HitModule::set_status_all(boma, app::HitStatus(*HIT_STATUS_NORMAL), 0);
     }
 }
 
