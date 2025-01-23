5d43c73abf68d03ec3d86b575a23530dad46d913
Askew <sqk9gw@gmail.com>
2025-01-23 05:33:53 -0500

    Revert "Pain"
    This reverts commit d2413d2492385938925f1d5179328fd0e875a316.

--- fighters/miigunner/src/groundbomb/mod.rs
+++ fighters/miigunner/src/groundbomb/mod.rs
@@ -1,13 +1,9 @@
 use super::*;
 
 mod acmd;
-mod opff;
-mod status;
 
 pub fn install() {
     let agent = &mut Agent::new("miigunner_groundbomb");
     acmd::install(agent);
-    opff::install(agent);
-    status::install(agent);
     agent.install();
 }
--- fighters/miigunner/src/supermissile/status.rs
+++ fighters/miigunner/src/supermissile/status.rs
@@ -2,6 +2,9 @@ use super::*;
 
 unsafe extern "C" fn straight_end(weapon: &mut L2CWeaponCommon) -> L2CValue {
     EffectModule::detach_all(weapon.module_accessor, 5);
+    println!("life: {}", weapon.get_int(*WEAPON_INSTANCE_WORK_ID_INT_LIFE));
+    println!("life <= 0? {}", weapon.get_int(*WEAPON_INSTANCE_WORK_ID_INT_LIFE) <= 0);
+    println!();
     if StatusModule::status_kind_next(weapon.module_accessor) == *WEAPON_MIIGUNNER_SUPERMISSILE_STATUS_KIND_S_BURST
     && weapon.get_int(*WEAPON_INSTANCE_WORK_ID_INT_LIFE) <= 0 {
         VarModule::on_flag(weapon.battle_object, vars::miigunner_supermissile::instance::PULSE_DETONATE);
