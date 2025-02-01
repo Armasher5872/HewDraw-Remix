e4d4ccdf7facaaacec075e2dd1ea13f9330437ce
Askew <sqk9gw@gmail.com>
2025-02-01 16:36:07 -0500

    Hey

--- fighters/dolly/src/acmd/specials/special_lw.rs
+++ fighters/dolly/src/acmd/specials/special_lw.rs
@@ -395,6 +395,7 @@ unsafe extern "C" fn sound_speciallwbreaking(agent: &mut L2CAgentBase) {
         PLAY_SE(agent, Hash40::new("vc_dolly_ottotto"));
         let handle = SoundModule::play_se_no3d(boma, Hash40::new("se_dolly_command_success"), true, true);
         SoundModule::set_se_vol(boma, handle as i32, 1.25, 0);
+        PLAY_SEQUENCE(agent, Hash40::new("seq_dolly_rnd_escape_f"));
     }
 }
 
