bdeefc56a00c8546608cfa53c0f298d82174c1e8
Askew <sqk9gw@gmail.com>
2025-02-04 22:11:55 -0500

    Update acmd.rs

--- fighters/pzenigame/src/water/acmd.rs
+++ fighters/pzenigame/src/water/acmd.rs
@@ -87,7 +87,7 @@ unsafe extern "C" fn game_clashpledgef(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     if is_excute(agent) {
-        ATTACK(agent, 0, 0, Hash40::new("top"), 10.0, 60, 120, 0, 35, 7.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, true, true, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_fire"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_WATER, *ATTACK_REGION_WATER);
+        ATTACK(agent, 0, 0, Hash40::new("top"), 10.0, 60, 108, 0, 35, 7.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, true, true, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_fire"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_WATER, *ATTACK_REGION_WATER);
         AttackModule::set_optional_hit_sound(boma, 0, Hash40::new("se_common_fire_l"));
     }
     frame(lua_state, 4.0);
