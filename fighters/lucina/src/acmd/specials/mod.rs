f504840103a36837873ade00c81375ca103f8419
Askew <sqk9gw@gmail.com>
2025-02-06 06:37:38 -0500

    Tiny misc

--- fighters/lucina/src/acmd/specials/mod.rs
+++ fighters/lucina/src/acmd/specials/mod.rs
@@ -287,6 +287,7 @@ unsafe extern "C" fn game_speciallw(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     frame(lua_state, 4.0);
+    FT_MOTION_RATE_RANGE(agent, 4.0, 15.0, 9.0);
     if is_excute(agent) {
         HitModule::set_hit_stop_mul(boma, 3.0, HitStopMulTarget{_address: (*HIT_STOP_MUL_TARGET_OPPONENT) as u8}, 0.0);
         DamageModule::set_damage_mul(boma, 0.5);
@@ -294,14 +295,15 @@ unsafe extern "C" fn game_speciallw(agent: &mut L2CAgentBase) {
         VarModule::set_float(boma.object(), vars::lucina::instance::CURRENT_DAMAGE, DamageModule::damage(boma, 0));
     }
     frame(lua_state, 15.0);
+    FT_MOTION_RATE(agent, 1.0);
     if is_excute(agent) {
         DamageModule::set_damage_mul(boma, 1.0);
         HitModule::set_hit_stop_mul(boma, 1.0, HitStopMulTarget{_address: (*HIT_STOP_MUL_TARGET_OPPONENT) as u8}, 0.0);
         damage!(agent, MA_MSC_DAMAGE_DAMAGE_NO_REACTION, DAMAGE_NO_REACTION_MODE_NORMAL, 0);
         VarModule::on_flag(agent.battle_object, vars::lucina::status::SPECIAL_LW_SPECIAL_CHECK);
     }
-    frame(lua_state, 28.0);
-    FT_MOTION_RATE_RANGE(agent, 28.0, 64.0, 12.0);
+    frame(lua_state, 26.0);
+    FT_MOTION_RATE_RANGE(agent, 26.0, 64.0, 16.0);
     if is_excute(agent) {
         VarModule::off_flag(agent.battle_object, vars::lucina::status::SPECIAL_LW_SPECIAL_CHECK);
     }
@@ -313,6 +315,7 @@ unsafe extern "C" fn game_specialairlw(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     frame(lua_state, 4.0);
+    FT_MOTION_RATE_RANGE(agent, 4.0, 15.0, 9.0);
     if is_excute(agent) {
         HitModule::set_hit_stop_mul(boma, 3.0, HitStopMulTarget{_address: (*HIT_STOP_MUL_TARGET_OPPONENT) as u8}, 0.0);
         DamageModule::set_damage_mul(boma, 0.5);
@@ -320,6 +323,7 @@ unsafe extern "C" fn game_specialairlw(agent: &mut L2CAgentBase) {
         VarModule::set_float(boma.object(), vars::lucina::instance::CURRENT_DAMAGE, DamageModule::damage(boma, 0));
     }
     frame(lua_state, 15.0);
+    FT_MOTION_RATE(agent, 1.0);
     if is_excute(agent) {
         DamageModule::set_damage_mul(boma, 1.0);
         HitModule::set_hit_stop_mul(boma, 1.0, HitStopMulTarget{_address: (*HIT_STOP_MUL_TARGET_OPPONENT) as u8}, 0.0);
@@ -327,9 +331,12 @@ unsafe extern "C" fn game_specialairlw(agent: &mut L2CAgentBase) {
         VarModule::on_flag(agent.battle_object, vars::lucina::status::SPECIAL_LW_SPECIAL_CHECK);
     }
     frame(lua_state, 28.0);
+    FT_MOTION_RATE_RANGE(agent, 28.0, 64.0, 30.0);
     if is_excute(agent) {
         VarModule::off_flag(agent.battle_object, vars::lucina::status::SPECIAL_LW_SPECIAL_CHECK);
     }
+    frame(lua_state, 64.0);
+    FT_MOTION_RATE(agent, 1.0);
 }
 
 pub fn install(agent: &mut Agent) {
