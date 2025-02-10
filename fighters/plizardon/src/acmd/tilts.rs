1c3741d18621dfe158c782920f77f77f8c134c8c
Askew <sqk9gw@gmail.com>
2025-02-10 03:58:08 -0500

    Lizardon

--- fighters/plizardon/src/acmd/tilts.rs
+++ fighters/plizardon/src/acmd/tilts.rs
@@ -115,7 +115,7 @@ unsafe extern "C" fn game_attacklw3(agent: &mut L2CAgentBase) {
         ATTACK(agent, 1, 0, Hash40::new("fire"), 12.0, 361, 94, 0, 45, 4.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_fire"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_FIRE, *ATTACK_REGION_TAIL);
         ATTACK(agent, 2, 0, Hash40::new("tail3"), 7.0, 361, 84, 0, 45, 4.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_TAIL);
     }
-    frame(lua_state, 14.0);
+    frame(lua_state, 15.0);
     if is_excute(agent) {
         AttackModule::clear_all(boma);
     }
