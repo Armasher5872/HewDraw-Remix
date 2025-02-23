22ed4bd7f9763bc72416aed6b199347d17378bb3
Askew <sqk9gw@gmail.com>
2025-02-23 09:23:30 -0500

    Disable swap lockout

--- fighters/ptrainer/src/ptrainer/opff.rs
+++ fighters/ptrainer/src/ptrainer/opff.rs
@@ -31,9 +31,9 @@ pub unsafe extern "C" fn pledge_timer(weapon: &mut L2CFighterBase) {
         }
     }
 
-    if is_training_mode() && VarModule::is_flag(weapon.battle_object, vars::ptrainer::instance::DISABLE_SPECIAL_LW) {
-        VarModule::off_flag(weapon.battle_object, vars::ptrainer::instance::DISABLE_SPECIAL_LW);
-    }
+    // if is_training_mode() && VarModule::is_flag(weapon.battle_object, vars::ptrainer::instance::DISABLE_SPECIAL_LW) {
+    //     VarModule::off_flag(weapon.battle_object, vars::ptrainer::instance::DISABLE_SPECIAL_LW);
+    // }
 }
 
 pub fn install(agent: &mut Agent) {
