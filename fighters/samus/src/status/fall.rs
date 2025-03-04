d64a4e1a6d0c2c7c81506661bca014df49c5dbfc
Askew <sqk9gw@gmail.com>
2025-03-04 14:07:37 -0500

    Zair tether fix

--- fighters/samus/src/status/fall.rs
+++ fighters/samus/src/status/fall.rs
@@ -1,5 +1,12 @@
 use super::*;
 
+unsafe extern "C" fn fall_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    if fighter.is_prev_status_one_of(&[*FIGHTER_STATUS_KIND_AIR_LASSO_HANG, *FIGHTER_STATUS_KIND_AIR_LASSO_REWIND]) {
+        VarModule::on_flag(fighter.battle_object, vars::common::instance::UP_SPECIAL_CANCEL);
+    }
+    smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_FALL)(fighter)
+}
+
 unsafe extern "C" fn landing_fall_special_init(fighter: &mut L2CFighterCommon) -> L2CValue {
     let ret = smashline::original_status(Init, fighter, *FIGHTER_STATUS_KIND_LANDING_FALL_SPECIAL)(fighter);
     if fighter.is_prev_status_one_of(&[*FIGHTER_SAMUS_STATUS_KIND_SPECIAL_S1A, *FIGHTER_SAMUS_STATUS_KIND_SPECIAL_S2A]) {
@@ -10,5 +17,6 @@ unsafe extern "C" fn landing_fall_special_init(fighter: &mut L2CFighterCommon) -
 }
 
 pub fn install(agent: &mut Agent) {
+    agent.status(Main, *FIGHTER_STATUS_KIND_FALL, fall_main);
     agent.status(Init, *FIGHTER_STATUS_KIND_LANDING_FALL_SPECIAL, landing_fall_special_init);
 }
