927b024a970759c1b1f7f39fc40983640f85c4b5
SuddyN <suddy121800@hotmail.com>
2025-03-06 18:27:04 -0500

    revert pledge on respawn

--- fighters/pfushigisou/src/status/mod.rs
+++ fighters/pfushigisou/src/status/mod.rs
@@ -7,6 +7,21 @@ mod special_n;
 mod special_s;
 
 unsafe extern "C" fn entry_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    reset_ptrainer_vars(fighter);
+    smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_ENTRY)(fighter)
+}
+
+unsafe extern "C" fn dead_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    reset_ptrainer_vars(fighter);
+    smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_DEAD)(fighter)
+}
+
+unsafe extern "C" fn rebirth_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    reset_ptrainer_vars(fighter);
+    smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_REBIRTH)(fighter)
+}
+
+unsafe extern "C" fn reset_ptrainer_vars(fighter: &mut L2CFighterCommon) {
     if LinkModule::is_link(fighter.module_accessor, *FIGHTER_POKEMON_LINK_NO_PTRAINER) {
         let parent_id = LinkModule::get_parent_id(fighter.module_accessor, *FIGHTER_POKEMON_LINK_NO_PTRAINER, true) as u32;
         let object = utils::util::get_battle_object_from_id(parent_id);
@@ -15,42 +30,8 @@ unsafe extern "C" fn entry_main(fighter: &mut L2CFighterCommon) -> L2CValue {
         VarModule::off_flag(object, vars::ptrainer::instance::SPECIAL_N_PLEDGE_PAUSE_TIMER);
         VarModule::set_int(object, vars::ptrainer::instance::SPECIAL_LW_SWAP_TIMER, 0);
     }
-    smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_ENTRY)(fighter)
 }
 
-unsafe extern "C" fn dead_main(fighter: &mut L2CFighterCommon) -> L2CValue {
-    if LinkModule::is_link(fighter.module_accessor, *FIGHTER_POKEMON_LINK_NO_PTRAINER) {
-        let parent_id = LinkModule::get_parent_id(fighter.module_accessor, *FIGHTER_POKEMON_LINK_NO_PTRAINER, true) as u32;
-        let object = utils::util::get_battle_object_from_id(parent_id);
-        VarModule::on_flag(object, vars::ptrainer::instance::SPECIAL_N_PLEDGE_PAUSE_TIMER);
-        VarModule::set_int(object, vars::ptrainer::instance::SPECIAL_LW_SWAP_TIMER, 0);
-        VarModule::set_int(object, vars::ptrainer::instance::SPECIAL_N_PLEDGE_STATE, *PLEDGE_STATE_GRASS);
-        let pledge_duration_frame = ParamModule::get_int(fighter.battle_object, ParamType::Agent, "param_special_lw.pledge_duration_frame");
-        VarModule::set_int(object, vars::ptrainer::instance::SPECIAL_N_PLEDGE_TIMER, pledge_duration_frame);
-    }
-    smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_DEAD)(fighter)
-}
-
-unsafe extern "C" fn rebirth_main(fighter: &mut L2CFighterCommon) -> L2CValue {
-    if LinkModule::is_link(fighter.module_accessor, *FIGHTER_POKEMON_LINK_NO_PTRAINER) {
-        let parent_id = LinkModule::get_parent_id(fighter.module_accessor, *FIGHTER_POKEMON_LINK_NO_PTRAINER, true) as u32;
-        let object = utils::util::get_battle_object_from_id(parent_id);
-        VarModule::set_int(object, vars::ptrainer::instance::SPECIAL_LW_SWAP_TIMER, 0);
-        let pledge_state = VarModule::get_int(object, vars::ptrainer::instance::SPECIAL_N_PLEDGE_STATE);
-        if pledge_state == *PLEDGE_STATE_WATER {
-            let handle = EffectModule::req_follow(fighter.module_accessor, Hash40::new("sys_status_defense_up"), Hash40::new("hip"), &Vector3f::new(0.7, 0.0, 0.0), &Vector3f::zero(), 0.8, true, 0, 0, 0, 0, 0, true, true) as u32;
-            VarModule::set_int(fighter.object(), vars::pfushigisou::instance::SPECIAL_N_PLEDGE_EFFECT_HANDLE, handle as i32);
-            fighter.play_pledge_effect(*PLEDGE_STATE_WATER);
-        }
-        else if pledge_state == *PLEDGE_STATE_FIRE {
-            let handle = EffectModule::req_follow(fighter.module_accessor, Hash40::new("sys_status_attack_up"), Hash40::new("hip"), &Vector3f::new(0.7, 0.0, 0.0), &Vector3f::zero(), 0.8, true, 0, 0, 0, 0, 0, true, true) as u32;
-            VarModule::set_int(fighter.object(), vars::pfushigisou::instance::SPECIAL_N_PLEDGE_EFFECT_HANDLE, handle as i32);
-            fighter.play_pledge_effect(*PLEDGE_STATE_FIRE);
-        }
-        VarModule::off_flag(object, vars::ptrainer::instance::SPECIAL_N_PLEDGE_PAUSE_TIMER);
-    }
-    smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_REBIRTH)(fighter)
-}
 pub fn install(agent: &mut Agent) {
     agent.status(Main, *FIGHTER_STATUS_KIND_ENTRY, entry_main);
     agent.status(Main, *FIGHTER_STATUS_KIND_DEAD, dead_main);
