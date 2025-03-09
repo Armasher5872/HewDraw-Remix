6947a7be3e39790bdf937a6f2b4661a84e65767b
SuddyN <suddy121800@hotmail.com>
2025-03-09 15:50:07 -0400

    zard pledge dthrow effects

--- fighters/plizardon/src/acmd/throws.rs
+++ fighters/plizardon/src/acmd/throws.rs
@@ -208,6 +208,123 @@ unsafe extern "C" fn game_throwlw(agent: &mut L2CAgentBase) {
     }
 }
 
+unsafe extern "C" fn effect_throwlw(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    let pledge = if LinkModule::is_link(boma, *FIGHTER_POKEMON_LINK_NO_PTRAINER) {
+        let parent_id = LinkModule::get_parent_id(boma, *FIGHTER_POKEMON_LINK_NO_PTRAINER, true) as u32;
+        let object = utils::util::get_battle_object_from_id(parent_id);
+        VarModule::get_int(object, vars::ptrainer::instance::SPECIAL_N_PLEDGE_STATE)
+    } else { *PLEDGE_STATE_NONE };
+    frame(lua_state, 11.0);
+    if pledge == *PLEDGE_STATE_WATER {
+        if is_excute(agent) {
+            EFFECT_FOLLOW_ALPHA(agent, Hash40::new("sys_steam2"), Hash40::new("head"), 0, 5, 0, 0, 0, 0, 0.7, false, 3.0);
+            EFFECT_FOLLOW(agent, Hash40::new("sys_drown_out"), Hash40::new("mouth2"), 0, 0, 0, 180, 0, 0, 0.6, false);
+        }
+    }
+    else if pledge == *PLEDGE_STATE_GRASS {
+        let mut handle = 0;
+        for _ in 0..2 {
+            if is_excute(agent) {
+                handle = EffectModule::req_follow(boma, Hash40::new("pfushigisou_atk_hi4"), Hash40::new("mouth2"), &Vector3f::zero(), &Vector3f::zero(), 0.0, false, 0, 0, 0, 0, 0, false, false);
+                EffectModule::set_rate(boma, handle as u32, 9.0);
+            }
+            wait(lua_state, 1.0);
+            if is_excute(agent) {
+                EffectModule::set_scale(boma, handle as u32, &Vector3f::new(0.45, 0.45, 0.45));
+                EffectModule::set_rate(boma, handle as u32, 0.5);
+                EffectModule::detach(boma, handle as u32, 0);
+            }
+            wait(lua_state, 2.0);
+        }
+    }
+    frame(lua_state, 18.0);
+    if is_excute(agent) {
+        let mut flame_color = Vector3f::new(1.0, 1.0, 1.0);
+        let mut flame_size = 1.0;
+        let mut flame_alpha = 1.0;
+        let mut flame_rate = 2.0;
+        if pledge == *PLEDGE_STATE_WATER {
+            EFFECT_OFF_KIND(agent, Hash40::new("sys_drown_out"), false, false);
+            EFFECT_FOLLOW_FLIP_ALPHA(agent, Hash40::new("sys_steam"), Hash40::new("sys_steam"), Hash40::new("top"), -1, 5, 10, 0, 0, 0, 1.0, true, *EF_FLIP_YZ, 5.0);
+            LAST_EFFECT_SET_SCALE_W(agent, 1.8, 1.5, 1.8);
+            LAST_EFFECT_SET_COLOR(agent, 0.7, 0.7, 0.74);
+            LAST_EFFECT_SET_RATE(agent, 0.8);
+            EFFECT_FOLLOW_ALPHA(agent, Hash40::new("sys_damage_fire"), Hash40::new("top"), 0, 8, 6, 0, 90, 0, 2, false, 1.0);
+            LAST_EFFECT_SET_COLOR(agent, 1.0, 1.0, 3.0);
+            // EFFECT_FOLLOW_ALPHA(agent, Hash40::new("sys_hit_fire"), Hash40::new("top"), 0, 8, 16, 0, 90, 0, 0.5, false, 0.8);
+            // LAST_EFFECT_SET_COLOR(agent, 0.5, 0.5, 3.0);
+
+            flame_color = Vector3f::new(0.6, 1.0, 6.0);
+            flame_size = 1.1;
+            flame_alpha = 0.85;
+        }
+        else if pledge == *PLEDGE_STATE_GRASS {
+            EFFECT_FOLLOW_FLIP_ALPHA(agent, Hash40::new("sys_steam"), Hash40::new("sys_steam"), Hash40::new("top"), -1, 5, 10, 0, 0, 0, 1.0, true, *EF_FLIP_YZ, 1.2);
+            LAST_EFFECT_SET_COLOR(agent, 2.0, 3.0, 0.5);
+            LAST_EFFECT_SET_RATE(agent, 0.7);
+            // EFFECT_FLIP(agent, Hash40::new("sys_grass_landing"), Hash40::new("sys_grass_landing"), Hash40::new("top"), -1, 5, 10, 0, 0, 0, 1.2, 0, 0, 0, 0, 0, 0, true, *EF_FLIP_YZ);
+            // LAST_EFFECT_SET_COLOR(agent, 4.0, 3.0, 1.0);
+            // LAST_EFFECT_SET_RATE(agent, 0.4);
+            EFFECT_FOLLOW_ALPHA(agent, Hash40::new("sys_damage_fire"), Hash40::new("top"), 0, 8, 6, 0, 90, 0, 2, false, 0.8);
+            LAST_EFFECT_SET_COLOR(agent, 0.6, 1.8, 1.0);
+
+            flame_color = Vector3f::new(0.3, 0.9, 0.5);
+            flame_size = 1.1;
+            flame_alpha = 0.7;
+            flame_rate = 1.0;
+        }
+
+        EFFECT_OFF_KIND(agent, Hash40::new("plizardon_flare_blitz_hold"), false, false);
+        EFFECT_FOLLOW_FLIP_ALPHA(agent, Hash40::new("plizardon_atk_mouth_fire"), Hash40::new("plizardon_atk_mouth_fire"), Hash40::new("head"), -1, 3, 0, 0, 180, 25, flame_size, true, *EF_FLIP_ROT_X, flame_alpha);
+        // EFFECT_FOLLOW_NO_STOP(agent, Hash40::new("plizardon_atk_mouth_fire"), Hash40::new("head"), -1, 3, 0, 0, 0, 25, 1.1, true);
+        LAST_EFFECT_SET_SCALE_W(agent, 1.2, 1.9, 1.2);
+        LAST_EFFECT_SET_COLOR(agent, flame_color.x, flame_color.y, flame_color.z);
+        LAST_EFFECT_SET_RATE(agent, flame_rate);
+    }
+    for _ in 0..3 {
+        if is_excute(agent) {
+            FOOT_EFFECT(agent, Hash40::new("sys_down_smoke"), Hash40::new("top"), 5, 0, 5, 0, 0, 0, 0.8, 0, 0, 5, 0, 0, 0, false);
+        }
+        wait(lua_state, 2.0);
+        if is_excute(agent) {
+            FLASH(agent, 1, 0.7, 0.4, 0.4);
+        }
+        wait(lua_state, 3.0);
+        if is_excute(agent) {
+            FLASH_FRM(agent, 2, 0, 0, 0, 0);
+        }
+        wait(lua_state, 4.0);
+    }
+    frame(lua_state, 48.0);
+    if is_excute(agent) {
+        EFFECT_OFF_KIND(agent, Hash40::new("plizardon_atk_mouth_fire"), false, false);
+    }
+    frame(lua_state, 49.0);
+    if is_excute(agent) {
+        let mut flame_color = Vector3f::new(1.0, 1.0, 1.0);
+        let mut flame_alpha = 1.0;
+        if pledge == *PLEDGE_STATE_WATER {
+            flame_color = Vector3f::new(0.6, 1.0, 6.0);
+            flame_alpha = 0.85;
+        }
+        else if pledge == *PLEDGE_STATE_GRASS {
+            flame_color = Vector3f::new(0.3, 0.9, 0.5);
+            flame_alpha = 0.7;
+        }
+        EFFECT_FOLLOW_FLIP_ALPHA(agent, Hash40::new("plizardon_atk_mouth_fire"), Hash40::new("plizardon_atk_mouth_fire"), Hash40::new("head"), -1, 3, 0, 0, 180, 25, 1.7, true, *EF_FLIP_ROT_X, flame_alpha);
+        LAST_EFFECT_SET_COLOR(agent, flame_color.x, flame_color.y, flame_color.z);
+        EFFECT(agent, Hash40::new("sys_crown"), Hash40::new("top"), 5, 0, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+        LANDING_EFFECT(agent, Hash40::new("null"), Hash40::new("top"), 5, 0, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+    }
+    frame(lua_state, 65.0);
+    if is_excute(agent) {
+        EFFECT_OFF_KIND(agent, Hash40::new("plizardon_atk_mouth_fire"), false, false);
+        EFFECT_OFF_KIND(agent, Hash40::new("sys_steam2"), false, false); 
+    }
+}
+
 pub fn install(agent: &mut Agent) {
     agent.acmd("game_catch", game_catch, Priority::Low);
     agent.acmd("game_catchdash", game_catchdash, Priority::Low);
@@ -217,4 +334,5 @@ pub fn install(agent: &mut Agent) {
     agent.acmd("game_throwb", game_throwb, Priority::Low);
     agent.acmd("game_throwhi", game_throwhi, Priority::Low);
     agent.acmd("game_throwlw", game_throwlw, Priority::Low);
+    agent.acmd("effect_throwlw", effect_throwlw, Priority::Low);
 }
