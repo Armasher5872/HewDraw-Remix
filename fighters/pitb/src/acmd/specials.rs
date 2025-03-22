7aec62a224bf321f3bbe909ba0aeb6ea5c64bf13
SuddyN <suddy121800@hotmail.com>
2025-03-22 16:49:57 -0400

    dark pit shine buff

--- fighters/pitb/src/acmd/specials.rs
+++ fighters/pitb/src/acmd/specials.rs
@@ -110,7 +110,10 @@ unsafe extern "C" fn game_speciallwstart(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     frame(lua_state, 1.0);
-    FT_MOTION_RATE_RANGE(agent, 1.0, 5.9, 2.0);
+    if is_excute(agent) {
+        sv_kinetic_energy!(set_speed, agent, FIGHTER_KINETIC_ENERGY_ID_GRAVITY, 0.0);
+    }
+    FT_MOTION_RATE_RANGE(agent, 1.0, 5.9, 1.0);
     frame(lua_state, 5.9);
     FT_MOTION_RATE_RANGE(agent, 5.9, 6.0, 1.0);
     if is_excute(agent) {
@@ -120,6 +123,7 @@ unsafe extern "C" fn game_speciallwstart(agent: &mut L2CAgentBase) {
     frame(lua_state, 6.0);
     FT_MOTION_RATE(agent, 1.0);
     if is_excute(agent) {
+        sv_kinetic_energy!(set_speed, agent, FIGHTER_KINETIC_ENERGY_ID_GRAVITY, 0.0);
         ATTACK(agent, 0, 0, Hash40::new("top"), 5.0, 70, 85, 0, 77, 7.5, 0.0, 7.0, -1.5, Some(0.0), Some(7.0), Some(1.5), 0.9, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_magic"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_MAGIC, *ATTACK_REGION_NONE);
     }
     frame(lua_state, 7.0);
