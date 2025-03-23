5753fd0e7bc66bcecb57fe7a6038a360b6be8fec
Askew <sqk9gw@gmail.com>
2025-03-23 19:49:55 -0400

    Printlns

--- fighters/miigunner/src/rapidshot_bullet/opff.rs
+++ fighters/miigunner/src/rapidshot_bullet/opff.rs
@@ -3,10 +3,10 @@ use super::*;
 
 pub extern "C" fn rapidshot_frame(weapon: &mut smash::lua2cpp::L2CFighterBase) {
     unsafe { 
-        println!("weapon motion: {}", weapon.is_motion(Hash40::new("fly")));
-        println!("weapon frame: {}", weapon.motion_frame());
+        //println!("weapon motion: {}", weapon.is_motion(Hash40::new("fly")));
+        //println!("weapon frame: {}", weapon.motion_frame());
         if weapon.is_motion(Hash40::new("fly")) && weapon.motion_frame() >= 7.0 {
-            println!("scaling");
+            //println!("scaling");
             ModelModule::set_scale(weapon.module_accessor, 0.4);
         }
     }
