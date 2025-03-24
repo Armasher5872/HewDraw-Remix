71ab40bd731266d37b7448557cbaf688eaef8b77
Askew <sqk9gw@gmail.com>
2025-03-24 01:48:19 -0400

    The Ganon Box

--- fighters/ganon/src/acmd/specials.rs
+++ fighters/ganon/src/acmd/specials.rs
@@ -6,6 +6,7 @@ unsafe extern "C" fn game_floatstart(agent: &mut L2CAgentBase) {
     frame(lua_state, 6.0);
     if is_excute(agent) {
         WHOLE_HIT(agent, *HIT_STATUS_XLU);
+        ATTACK(agent, 0, 0, Hash40::new("waist"), 10.0, 60, 80, 0, 45, 5.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 5, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_SWORD);
     }
     frame(lua_state, 8.0);
     if is_excute(agent) {
@@ -65,6 +66,10 @@ unsafe extern "C" fn game_floatairstart(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     FT_MOTION_RATE(agent, 1.0 / 10.0);
+    frame(lua_state, 6.0);
+    if is_excute(agent) {
+        ATTACK(agent, 0, 0, Hash40::new("waist"), 10.0, 60, 80, 0, 45, 5.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 5, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_SWORD);
+    }
     frame(lua_state, 9.0);
     FT_MOTION_RATE(agent, 1.0);
     KineticModule::unable_energy(boma, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
