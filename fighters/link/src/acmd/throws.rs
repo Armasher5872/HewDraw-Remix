2ad0d6d3e09b8d86209c5673aab51e0d88921846
ryn <ryanhunteralbert@gmail.com>
2025-05-23 01:17:57 -0500

    etc

--- fighters/link/src/acmd/throws.rs
+++ fighters/link/src/acmd/throws.rs
@@ -121,7 +121,7 @@ unsafe extern "C" fn game_throwhi(agent: &mut L2CAgentBase) {
         ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 40, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
     }
     frame(lua_state, 12.0);
-    FT_MOTION_RATE_RANGE(agent, 12.0, 25.0, 15.0);
+    FT_MOTION_RATE_RANGE(agent, 12.0, 25.0, 14.0);
     frame(lua_state, 25.0);
     FT_MOTION_RATE(agent, 1.0);
     frame(lua_state, 26.0);
