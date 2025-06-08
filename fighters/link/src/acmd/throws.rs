00c1668692a18454c9a9f43dae414af2ea587c2c
ryn <ryanhunteralbert@gmail.com>
2025-06-07 19:28:56 -0500

    lag

--- fighters/link/src/acmd/throws.rs
+++ fighters/link/src/acmd/throws.rs
@@ -153,7 +153,7 @@ unsafe extern "C" fn game_throwlw(agent: &mut L2CAgentBase) {
         ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 40, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
     }
     frame(lua_state, 5.0);
-    FT_MOTION_RATE_RANGE(agent, 5.0, 21.0, 10.0);
+    FT_MOTION_RATE_RANGE(agent, 5.0, 21.0, 9.0);
     frame(lua_state, 21.0);
     FT_MOTION_RATE(agent, 1.0);
     frame(lua_state, 22.0);
@@ -163,7 +163,7 @@ unsafe extern "C" fn game_throwlw(agent: &mut L2CAgentBase) {
         CHECK_FINISH_CAMERA(agent, -2, 0);
     }
     frame(lua_state, 24.0);
-    FT_MOTION_RATE_RANGE(agent, 24.0, 48.0, 28.0);
+    FT_MOTION_RATE_RANGE(agent, 24.0, 48.0, 26.0);
     if is_excute(agent) {
         let target = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT);
         let target_group = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP);
