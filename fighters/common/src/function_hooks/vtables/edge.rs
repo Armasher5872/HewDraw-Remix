4e708421dfbc80bd99d43002cb95f128755ddac2
WuBoytH <alexwuief@gmail.com>
2025-06-25 12:53:10 -0400

    13.0.4 offset

--- fighters/common/src/function_hooks/vtables/edge.rs
+++ fighters/common/src/function_hooks/vtables/edge.rs
@@ -46,8 +46,8 @@ unsafe fn edge_flash_on_search(vtable: u64, weapon: &mut app::Weapon, log: *mut 
 pub fn install() {
     unsafe {
         let text = skyline::hooks::getRegionAddress(skyline::hooks::Region::Text) as *mut u64;
-        let _ = skyline::patching::Patch::in_text(0x518a860 + (*WEAPON_KIND_EDGE_FLASH as usize * 0x1d * 0x8)).data(text.add(0x33b8cf0 / 0x8));
+        let _ = skyline::patching::Patch::in_text(0x5189860 + (*WEAPON_KIND_EDGE_FLASH as usize * 0x1d * 0x8)).data(text.add(0x33b8a80 / 0x8));
     }
 
-    let _ = skyline::patching::Patch::in_text(0x51c1ff0).data(edge_flash_on_search as u64);
+    let _ = skyline::patching::Patch::in_text(0x51c0ff0).data(edge_flash_on_search as u64);
 }
