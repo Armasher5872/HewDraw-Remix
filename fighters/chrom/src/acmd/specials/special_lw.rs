06d0ccd5337ea49e574d481ee005dd9c8653438a
SuddyN <suddy121800@hotmail.com>
2025-07-04 16:32:10 -0400

    dspecial VFX/SFX and momentum improvements

--- fighters/chrom/src/acmd/specials/special_lw.rs
+++ fighters/chrom/src/acmd/specials/special_lw.rs
@@ -8,9 +8,14 @@ unsafe extern "C" fn game_speciallw(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_NONE);
     }
-    FT_MOTION_RATE_RANGE(agent, 1.0, 15.0, 10.0);
-    frame(lua_state, 15.0);
-    FT_MOTION_RATE(agent, 1.0);
+    frame(lua_state, 6.0);
+    if is_excute(agent) {
+        damage!(agent, *MA_MSC_DAMAGE_DAMAGE_NO_REACTION, *DAMAGE_NO_REACTION_MODE_ALWAYS, 0);
+    }
+    frame(lua_state, 18.0);
+    if is_excute(agent) {
+        damage!(agent, *MA_MSC_DAMAGE_DAMAGE_NO_REACTION, *DAMAGE_NO_REACTION_MODE_NORMAL, 0);
+    }
     frame(lua_state, 22.0);
     let rate = 10.0 / 16.0;
     FT_MOTION_RATE(agent, rate);
@@ -23,13 +28,101 @@ unsafe extern "C" fn game_speciallw(agent: &mut L2CAgentBase) {
     sv_kinetic_energy!(set_speed_mul, agent, FIGHTER_KINETIC_ENERGY_ID_MOTION, 0.9);
     if is_excute(agent) {
         AttackModule::clear_all(boma);
-        if agent.is_situation(*SITUATION_KIND_AIR) {
-            KineticModule::enable_energy(boma, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
-        }
+    }
+    frame(lua_state, 44.0);
+    let rate = 26.0 / 32.0;
+    FT_MOTION_RATE(agent, rate);
+    sv_kinetic_energy!(set_speed_mul, agent, FIGHTER_KINETIC_ENERGY_ID_MOTION, rate * 0.9);
+    if is_excute(agent) {
+        notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_ON_DROP);
+    }
+}
+
+unsafe extern "C" fn game_specialairlw(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    frame(lua_state, 1.0);
+    sv_kinetic_energy!(set_speed_mul, agent, FIGHTER_KINETIC_ENERGY_ID_MOTION, 0.1);
+    if is_excute(agent) {
+        notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_NONE);
+    }
+    frame(lua_state, 22.0);
+    let rate = 10.0 / 16.0;
+    FT_MOTION_RATE(agent, rate);
+    sv_kinetic_energy!(set_speed_mul, agent, FIGHTER_KINETIC_ENERGY_ID_MOTION, rate * 0.9);
+    if is_excute(agent) {
+        ATTACK(agent, 0, 0, Hash40::new("top"), 0.0, 0, 0, 0, 0, 4.0, 0.0, 4.0, 5.0, Some(0.0), Some(4.0), Some(9.0), 0.0, 0.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, true, f32::NAN, 0.0, 0, false, false, true, true, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_FIGHTER, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_none"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_NONE);
+    }
+    frame(lua_state, 38.0);
+    FT_MOTION_RATE(agent, 1.0);
+    sv_kinetic_energy!(set_speed_mul, agent, FIGHTER_KINETIC_ENERGY_ID_MOTION, 0.9);
+    if is_excute(agent) {
+        AttackModule::clear_all(boma);
+        KineticModule::enable_energy(boma, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
     }
     frame(lua_state, 44.0);
     if is_excute(agent) {
         notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_ON_DROP);
+    }
+}
+
+unsafe extern "C" fn effect_speciallw(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    frame(lua_state, 6.0);
+    if is_excute(agent) {
+        EFFECT(agent, Hash40::new("chrom_counter_flash"), Hash40::new("sword1"), 0, 2, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, true);
+        FLASH(agent, 1, 1, 1, 0.75);
+    }
+
+    // flashing effect
+    frame(lua_state, 7.0);
+    for _ in 0..3 {
+        if is_excute(agent) {
+            FLASH(agent, 0.7, 0.7, 0.7, 0.5);
+        }
+        wait(lua_state, 2.0);
+        if is_excute(agent) {
+            FLASH(agent, 0.67, 0, 0.78, 0.31);
+        }
+        wait(lua_state, 2.0);
+        if is_excute(agent) {
+            COL_NORMAL(agent);
+        }
+        wait(lua_state, 1.0);
+    }
+
+    // dash smoke
+    frame(lua_state, 22.0);
+    if agent.is_situation(*SITUATION_KIND_GROUND) {
+        if is_excute(agent) {
+            LANDING_EFFECT(agent, Hash40::new("sys_dash_smoke"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+        }
+        for _ in 0..4 {
+            if is_excute(agent) {
+                FOOT_EFFECT(agent, Hash40::new("sys_run_smoke"), Hash40::new("top"), 2, 0, 0, 0, 0, 0, 1, 2, 0, 4, 0, 0, 0, false);
+            }
+            wait(lua_state, 4.0);
+        }
+    } else {
+        if is_excute(agent) {
+            EFFECT_FOLLOW(agent, Hash40::new("sys_attack_speedline"), Hash40::new("top"), 0, 15, -12, 30, 0, 0, 1.0, true);
+            LAST_EFFECT_SET_RATE(agent, 0.4);
+        }
+    }
+}
+
+unsafe extern "C" fn sound_speciallw(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    frame(lua_state, 6.0);
+    if is_excute(agent) {
+        PLAY_SE(agent, Hash40::new("se_chrom_special_l01"));
+    }
+    frame(lua_state, 22.0);
+    if is_excute(agent) {
+        PLAY_SE(agent, Hash40::new("se_chrom_escape"));
+        PLAY_SEQUENCE(agent, Hash40::new("seq_chrom_rnd_attack"));
     }
 }
 
@@ -67,7 +160,12 @@ unsafe extern "C" fn game_speciallwhit(agent: &mut L2CAgentBase) {
 
 pub fn install(agent: &mut Agent) {
     agent.acmd("game_speciallw", game_speciallw, Priority::Low);
-    agent.acmd("game_specialairlw", game_speciallw, Priority::Low);
+    agent.acmd("game_specialairlw", game_specialairlw, Priority::Low);
+    agent.acmd("effect_speciallw", effect_speciallw, Priority::Low);
+    agent.acmd("effect_specialairlw", effect_speciallw, Priority::Low);
+    agent.acmd("sound_speciallw", sound_speciallw, Priority::Low);
+    agent.acmd("sound_specialairlw", sound_speciallw, Priority::Low);
+    
     agent.acmd("game_speciallwhit", game_speciallwhit, Priority::Low);
     agent.acmd("game_specialairlwhit", game_speciallwhit, Priority::Low);
 }
