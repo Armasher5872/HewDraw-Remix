02580979871300913bff83d475498192a96ef9d3
SuddyN <suddy121800@hotmail.com>
2025-07-19 19:18:25 -0400

    fix DAir spike accuracy

--- fighters/pichu/src/acmd/aerials.rs
+++ fighters/pichu/src/acmd/aerials.rs
@@ -181,7 +181,7 @@ unsafe extern "C" fn game_attackairlw(agent: &mut L2CAgentBase) {
         HIT_NODE(agent, Hash40::new("mimil1"), *HIT_STATUS_XLU);
         PICHU_ADD_DAMAGE(agent, 2.0);
         MeterModule::watch_damage(agent.battle_object, true);
-        ATTACK(agent, 0, 0, Hash40::new("neck"), 13.0 * damage_mul,  60, 89, 0, 40, 4.5, 0.0,  0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_HEAD);
+        ATTACK(agent, 0, 0, Hash40::new("neck"), 13.0 * damage_mul,  60, 89, 0, 40, 3.5, 0.0,  0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_HEAD);
         ATTACK(agent, 1, 0, Hash40::new("neck"), 14.0 * damage_mul, 270, 47, 0, 23, 5.5, 4.5, -1.3, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_HEAD);
     }
     frame(lua_state, 15.0);
