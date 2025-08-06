ac1a6b076e422363e0f8f4cfdb4309d357db8e4b
jaccob101 <jacobking101007@gmail.com>
2025-08-05 21:13:49 -0400

    Update special_n.rs

--- fighters/peach/src/status/special_n.rs
+++ fighters/peach/src/status/special_n.rs
@@ -6,6 +6,7 @@ unsafe extern "C" fn special_n_main(fighter: &mut L2CFighterCommon) -> L2CValue 
     let ret = smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_SPECIAL_N)(fighter);
     // air stall wasnt working
     if !fighter.is_situation(*SITUATION_KIND_GROUND) {
+        let special_n_attack_speed_y = fighter.get_param_float("param_special_n", "special_n_attack_speed_y");
         sv_kinetic_energy!(set_speed, fighter, FIGHTER_KINETIC_ENERGY_ID_GRAVITY, special_n_attack_speed_y);
         let special_n_attack_stable_y = fighter.get_param_float("param_special_n", "special_n_attack_stable_y");
         sv_kinetic_energy!(set_stable_speed, fighter, FIGHTER_KINETIC_ENERGY_ID_GRAVITY, -special_n_attack_stable_y);
