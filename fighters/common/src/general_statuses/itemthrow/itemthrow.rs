d6925f22256aedb83c522f757a5194d6111a63e5
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2025-08-25 09:58:48 -0400

    increase min ditcit speed

--- fighters/common/src/general_statuses/itemthrow/itemthrow.rs
+++ fighters/common/src/general_statuses/itemthrow/itemthrow.rs
@@ -9,37 +9,45 @@ pub fn install() {
 fn nro_hook(info: &skyline::nro::NroInfo) {
     if info.name == "common" {
         skyline::install_hooks!(
-            status_ItemThrowCommon,
+            status_ItemThrow_Main,
         );
     }
 }
 
-#[skyline::hook(replace = smash::lua2cpp::L2CFighterCommon_status_ItemThrowCommon)]
-unsafe fn status_ItemThrowCommon(fighter: &mut L2CFighterCommon) {
-    call_original!(fighter);
+#[skyline::hook(replace = smash::lua2cpp::L2CFighterCommon_status_ItemThrow_Main)]
+unsafe fn status_ItemThrow_Main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    if StatusModule::is_changing(fighter.module_accessor) {
+        if VarModule::is_flag(fighter.battle_object, vars::common::instance::IS_DITCIT) {
+            VarModule::off_flag(fighter.battle_object, vars::common::instance::IS_DITCIT);
 
-    if VarModule::is_flag(fighter.battle_object, vars::common::instance::IS_DITCIT) {
-        let mut speed_x = fighter.get_speed_x(*FIGHTER_KINETIC_ENERGY_ID_MOTION);
-        let speed_y = fighter.get_speed_y(*FIGHTER_KINETIC_ENERGY_ID_MOTION);
-        let min_speed = ParamModule::get_float(fighter.battle_object, ParamType::Common, "ditcit.min_speed");
-        let max_speed = ParamModule::get_float(fighter.battle_object, ParamType::Common, "ditcit.max_speed");
-        let ditcit_mul = ParamModule::get_float(fighter.object(), ParamType::Shared, "ditcit_mul");
+            let mut speed_x = fighter.get_speed_x(*FIGHTER_KINETIC_ENERGY_ID_MOTION);
+            let speed_y = fighter.get_speed_y(*FIGHTER_KINETIC_ENERGY_ID_MOTION);
+            let min_speed = ParamModule::get_float(fighter.battle_object, ParamType::Common, "ditcit.min_speed");
+            let max_speed = ParamModule::get_float(fighter.battle_object, ParamType::Common, "ditcit.max_speed");
+            let ditcit_mul = ParamModule::get_float(fighter.object(), ParamType::Shared, "ditcit_mul");
 
-        speed_x = speed_x * ditcit_mul;
+            speed_x = speed_x * ditcit_mul;
 
-        if min_speed > 0.0 {
-            if speed_x == 0.0 {
-                speed_x = min_speed * PostureModule::lr(fighter.module_accessor);
+            if min_speed > 0.0 {
+                println!("ok");
+                if speed_x == 0.0 {
+                    println!("none");
+                    speed_x = min_speed * PostureModule::lr(fighter.module_accessor);
+                }
+                else {
+                    speed_x = speed_x.abs().max(min_speed) * speed_x.signum();
+                }
             }
-            else {
-                speed_x = speed_x.abs().max(min_speed) * speed_x.signum();
+
+            if max_speed > 0.0 {
+                speed_x = speed_x.abs().min(max_speed) * speed_x.signum();
             }
-        }
 
-        if max_speed > 0.0 {
-            speed_x = speed_x.abs().min(max_speed) * speed_x.signum();
-        }
+            println!("{}", speed_x);
 
-        sv_kinetic_energy!(set_speed, fighter, FIGHTER_KINETIC_ENERGY_ID_MOTION, speed_x, speed_y);
+            sv_kinetic_energy!(set_speed, fighter, FIGHTER_KINETIC_ENERGY_ID_MOTION, speed_x, speed_y);
+        }
     }
+    
+    call_original!(fighter)
 }
