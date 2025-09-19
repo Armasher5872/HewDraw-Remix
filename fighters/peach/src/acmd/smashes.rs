f9b42906cccb1a4b6ab8e1fc515ef582877e765f
jaccob101 <jacobking101007@gmail.com>
2025-09-19 08:35:49 -0400

    brighter blue spin gfx

--- fighters/peach/src/acmd/smashes.rs
+++ fighters/peach/src/acmd/smashes.rs
@@ -140,31 +140,31 @@ unsafe extern "C" fn effect_attacklw4(agent: &mut L2CAgentBase) {
     frame(lua_state, 4.0);
     if is_excute(agent) {
         EFFECT_FOLLOW(agent, Hash40::new("peach_smash_arc"), Hash40::new("top"), 0, 1.8, 0.0, 90.0 + 90.0*boma.lr(), -55, 0, 1.0, true);
-        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.0, 2.0);
+        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.33, 2.0);
         LAST_EFFECT_SET_SCALE_W(agent, 0.95, 1.55, 0.95);
     }
     frame(lua_state, 8.0);
     if is_excute(agent) {
         EFFECT_FOLLOW(agent, Hash40::new("peach_smash_arc"), Hash40::new("top"), 0, 1.8, 0, 90.0 + 90.0*boma.lr(), 100, 0, 1.0, true);
-        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.2, 2.0);
+        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.33, 2.0);
         LAST_EFFECT_SET_SCALE_W(agent, 0.95, 1.55, 0.95);
     }
     frame(lua_state, 12.0);
     if is_excute(agent) {
         EFFECT_FOLLOW(agent, Hash40::new("peach_smash_arc"), Hash40::new("top"), 0, 1.8, 0, 90.0 + 90.0*boma.lr(), -110, 0, 1.0, true);
-        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.2, 2.0);
+        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.33, 2.0);
         LAST_EFFECT_SET_SCALE_W(agent, 0.95, 1.55, 0.95);
     }
     frame(lua_state, 15.0);
     if is_excute(agent) {
         EFFECT_FOLLOW(agent, Hash40::new("peach_smash_arc"), Hash40::new("top"), 0, 1.8, 0.0, 90.0 + 90.0*boma.lr(), 120, 0, 1.0, true);
-        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.2, 2.0);
+        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.33, 2.0);
         LAST_EFFECT_SET_SCALE_W(agent, 0.95, 1.55, 0.95);
     }
     frame(lua_state, 19.0);
     if is_excute(agent) {
         EFFECT_FOLLOW(agent, Hash40::new("peach_smash_arc"), Hash40::new("top"), 0, 1.8, 0, 90.0 + 90.0*boma.lr(), -110, 0, 1.0, true);
-        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.2, 2.0);
+        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.33, 2.0);
         LAST_EFFECT_SET_SCALE_W(agent, 0.95, 1.55, 0.95);
     }
     frame(lua_state, 24.0);
@@ -176,7 +176,7 @@ unsafe extern "C" fn effect_attacklw4(agent: &mut L2CAgentBase) {
     frame(lua_state, 25.0);
     if is_excute(agent) {
         EFFECT_FOLLOW(agent, Hash40::new("peach_smash_arc"), Hash40::new("top"), 0, 1.8, 0, 90.0 + 90.0*boma.lr(), 115, 0, 1.0, true);
-        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.2, 2.0);
+        LAST_EFFECT_SET_COLOR(agent, 0.2, 1.33, 2.0);
         LAST_EFFECT_SET_SCALE_W(agent, 0.98, 1.55, 0.98);
         LAST_EFFECT_SET_RATE(agent, 0.75);
     }
