fe0e219a35c6bc33a2582258121a72ca6c3cf761
4Lukaska <79385530+4Lukaska@users.noreply.github.com>
2025-09-20 14:57:27 -0400

    kirby fix

--- fighters/kirby/src/acmd/copy/mario.rs
+++ fighters/kirby/src/acmd/copy/mario.rs
@@ -93,19 +93,16 @@ unsafe extern "C" fn effect_mariospecialnfire(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         EFFECT(agent, Hash40::new("sys_smash_flash_s"), Hash40::new("top"), 6, 11, 0, 0, 0, 0, 0.9, 0, 0, 0, 0, 0, 0, false);
     }
-    frame(lua_state, 11.0);
+    frame(lua_state, 13.0);
     if is_excute(agent) {
         if PostureModule::lr(boma) > 0.0 {
-            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("havel"), 0, 0, 0, 0, 45, 0, 0.7, true);
-            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("haver"), 0, 0, 0, 0, 45, 0, 0.7, true);
+            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("havel"), 0, 0, 0, 0, 45, 0, 0.55, true);
+            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("haver"), 0, 0, 0, 0, 45, 0, 0.55, true);
         }
         else {
-            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("havel"), 0, 0, 0, 0, -45, 0, 0.7, true);
-            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("haver"), 0, 0, 0, 0, -45, 0, 0.7, true);
+            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("havel"), 0, 0, 0, 0, -45, 0, 0.55, true);
+            EFFECT_FOLLOW(agent, Hash40::new("mario_fb_shoot"), Hash40::new("haver"), 0, 0, 0, 0, -45, 0, 0.55, true);
         }
-    }
-    frame(lua_state, 12.0);
-    if is_excute(agent) {
         if agent.is_situation(*SITUATION_KIND_GROUND) {
             EFFECT_FOLLOW(agent, Hash40::new("sys_h_smoke_b"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 0.5, true);
             LAST_EFFECT_SET_COLOR(agent, 0.2, 0.2, 0.2);
@@ -114,10 +111,13 @@ unsafe extern "C" fn effect_mariospecialnfire(agent: &mut L2CAgentBase) {
     frame(lua_state, 14.0);
     if is_excute(agent) {
         FLASH(agent, 1, 0, 0, 0.35);
-        EFFECT_FOLLOW(agent, Hash40::new("sys_flame"), Hash40::new("handl"), 0.0, 0, 0, 0, 0, 0, 0.2, true);
-        EFFECT_FOLLOW(agent, Hash40::new("sys_flame"), Hash40::new("handr"), 0.0, 0, 0, 0, 0, 0, 0.2, true);
-        EFFECT_FOLLOW(agent, Hash40::new("sys_bomb_a"), Hash40::new("top"), 0, 6.5, 11.5, 0, 0, 0, 0.26, true);
-        LAST_EFFECT_SET_RATE(agent, 1.2);
+        EFFECT_FOLLOW(agent, Hash40::new("sys_bomb_a"), Hash40::new("top"), 1, 7.0, 10.5, 0, 0, 0, 0.26, true);
+        LAST_EFFECT_SET_COLOR(agent, 0.65, 0.2, 0.08);
+        LAST_EFFECT_SET_RATE(agent, 0.75);
+        EFFECT_FOLLOW(agent, Hash40::new("sys_damage_fire"), Hash40::new("top"), 1, 6.0, 10.5, 0, 0, 0, 0.9, true);
+        LAST_EFFECT_SET_RATE(agent, 0.55);
+        EFFECT_FOLLOW(agent, Hash40::new("sys_damage_fire"), Hash40::new("top"), 1, 5.0, 10.5, 0, 0, 0, 0.9, true);
+        LAST_EFFECT_SET_RATE(agent, 0.55);
         EffectModule::enable_sync_init_pos_last(boma);
         if agent.is_situation(*SITUATION_KIND_GROUND) {
             EFFECT_FOLLOW(agent, Hash40::new("sys_h_smoke_b"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 0.5, true);
--- fighters/mario/src/acmd/other.rs
+++ fighters/mario/src/acmd/other.rs
@@ -84,7 +84,7 @@ unsafe extern "C" fn sound_jumpfront(agent: &mut L2CAgentBase) {
         let rng = app::sv_math::rand(hash40("fighter"), 2);
         if rng == 0 {
             let handle = SoundModule::play_se(agent.module_accessor, Hash40::new("vc_mario_jump01"), false, false, false, false, enSEType(0)); //hooh
-            SoundModule::set_se_vol(agent.module_accessor, handle as i32, 0.75, 0);
+            SoundModule::set_se_vol(agent.module_accessor, handle as i32, 0.70, 0);
         }
     }
     wait(lua_state, 1.0);
@@ -189,12 +189,12 @@ pub fn install(agent: &mut Agent) {
     agent.acmd("sound_damageflytop", sound_damagefly, Priority::Low);
     agent.acmd("sound_damageflyroll", sound_damageflyroll, Priority::Low);
 
-    agent.acmd("sound_jumpfront", sound_jumpfront, Priority::Low);
-    agent.acmd("sound_jumpback", sound_jumpfront, Priority::Low);
-
     agent.acmd("game_dash", game_dash, Priority::Low);
     agent.acmd("sound_dash", sound_dash, Priority::Low);
     agent.acmd("game_turndash", game_turndash, Priority::Low);
+
+    agent.acmd("sound_jumpfront", sound_jumpfront, Priority::Low);
+    agent.acmd("sound_jumpback", sound_jumpfront, Priority::Low);
 
     agent.acmd("game_escapeair", game_escapeair, Priority::Low);
     agent.acmd("game_escapeairslide", game_escapeairslide, Priority::Low);
