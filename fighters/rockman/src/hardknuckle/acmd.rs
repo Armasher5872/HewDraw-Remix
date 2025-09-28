75ce0a516f6fa1926c41203056156a91c43f6305
WuBoytH <alexwuief@gmail.com>
2025-09-28 14:02:20 -0400

    Hard Knuckle Adjustments

--- fighters/rockman/src/hardknuckle/acmd.rs
+++ fighters/rockman/src/hardknuckle/acmd.rs
@@ -14,8 +14,8 @@ unsafe extern "C" fn game_regular(agent: &mut L2CAgentBase) {
 	if is_excute(agent) {
         ATTACK(agent, 0, 0, Hash40::new("top"), 6.0, 90, 20, 0, 30, 4.5, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_THRU, *ATTACK_LR_CHECK_SPEED, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_PUNCH);
         //sv_kinetic_energy!(set_speed, agent, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, 0.0, -0.5);
-        sv_kinetic_energy!(set_stable_speed, agent, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, 0.0, 0.0);
-        sv_kinetic_energy!(set_brake, agent, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, 0.0, 0.8);
+        sv_kinetic_energy!(set_stable_speed, agent, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, 0.0, 1.0);
+        sv_kinetic_energy!(set_brake, agent, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, 0.0, 0.3);
     }
 }
 
@@ -32,7 +32,29 @@ unsafe extern "C" fn effect_regular(agent: &mut L2CAgentBase) {
     // }
 }
 
+unsafe extern "C" fn game_stick(agent: &mut L2CAgentBase) {
+    frame(agent.lua_state_agent, 1.0);
+    if is_excute(agent) {
+        QUAKE(agent, *CAMERA_QUAKE_KIND_M);
+        ATTACK(agent, 0, 0, Hash40::new("top"), 4.0, 75, 50, 0, 50, 1.0, 8.0, 0.0, 0.0, Some(-8.0), Some(0.0), Some(0.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_PUNCH);
+    }
+    wait(agent.lua_state_agent, 2.0);
+    if is_excute(agent) {
+        AttackModule::clear_all(agent.module_accessor);
+    }
+}
+
+unsafe extern "C" fn effect_stick(agent: &mut L2CAgentBase) {
+    frame(agent.lua_state_agent, 1.0);
+    if is_excute(agent) {
+        EFFECT(agent, Hash40::new("sys_crown"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 0.75, 0, 0, 0, 0, 0, 0, false);
+    }
+}
+
 pub fn install(agent: &mut Agent) {
     agent.acmd("game_regular", game_regular, Priority::Low);
     agent.acmd("effect_regular", effect_regular, Priority::Low);
+
+    agent.acmd("game_stick", game_stick, Priority::Low);
+    agent.acmd("effect_stick", effect_stick, Priority::Low);
 }
