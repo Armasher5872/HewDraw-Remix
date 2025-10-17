7c70f2fbb311d42e56974ad52865cde6121f1e0a
SuddyN <suddy121800@hotmail.com>
2025-10-17 19:37:00 -0400

    fix villy buffered ledgedash

--- fighters/murabito/src/acmd/other.rs
+++ fighters/murabito/src/acmd/other.rs
@@ -73,6 +73,14 @@ unsafe extern "C" fn game_itemlightthrowdash(agent: &mut L2CAgentBase) {
     }
 }
 
+unsafe extern "C" fn game_cliffjump2(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if is_excute(agent) {
+        PostureModule::add_pos(boma, &Vector3f::new(0.0, -2.0, 0.0));
+    }
+}
+
 pub fn install(agent: &mut Agent) {
     agent.acmd("game_cliffescape", acmd_stub, Priority::Low);
 
@@ -84,4 +92,6 @@ pub fn install(agent: &mut Agent) {
     agent.acmd("game_escapeairslide", game_escapeairslide, Priority::Low);
 
     agent.acmd("game_itemlightthrowdash", game_itemlightthrowdash, Priority::Low);
+
+    agent.acmd("game_cliffjump2", game_cliffjump2, Priority::Low);
 }
