2b951ffbcbbd55cd8651de82f53463e9d44842f4
jaccob101 <jacobking101007@gmail.com>
2025-10-18 19:03:14 -0400

    a

--- fighters/peach/src/acmd/tilts.rs
+++ fighters/peach/src/acmd/tilts.rs
@@ -77,7 +77,7 @@ unsafe extern "C" fn game_attacklw3(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     frame(lua_state, 1.0);
-    FT_MOTION_RATE(agent, 4.0/(6.0-1.0));
+    FT_MOTION_RATE_RANGE(agent, 1.0, 6.0, 3.0);
     if is_excute(agent) {
         FighterAreaModuleImpl::enable_fix_jostle_area(boma, 4.0, 4.0);
     }
@@ -93,13 +93,13 @@ unsafe extern "C" fn game_attacklw3(agent: &mut L2CAgentBase) {
         ATTACK(agent, 3, 0, Hash40::new("handl"), 10.0, 85, 95, 0, 42, 4.0, 1.0, 0.0, -0.3, None, None, None, 1.1, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_A, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_SLAP, *ATTACK_REGION_PUNCH);
         AttackModule::set_attack_height_all(boma, app::AttackHeight(*ATTACK_HEIGHT_LOW), false);
     }
-    frame(lua_state, 14.0); // 13
+    frame(lua_state, 14.0); // 12
     FT_MOTION_RATE_RANGE(agent, 14.0, 28.0, 12.0);
     if is_excute(agent) {
         AttackModule::clear_all(boma);
         FighterAreaModuleImpl::enable_fix_jostle_area(boma, 3.0, 3.0);
     }
-    frame(lua_state, 28.0); // 25
+    frame(lua_state, 28.0); // 24
     FT_MOTION_RATE(agent, 1.0);
 }
 
