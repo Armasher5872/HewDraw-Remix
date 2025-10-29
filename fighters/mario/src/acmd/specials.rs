360c44c399ab0bfc55d385d65aaf4d248d966d4b
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2025-10-29 19:08:50 -0400

    slightly reduce marios upB hangtime

--- fighters/mario/src/acmd/specials.rs
+++ fighters/mario/src/acmd/specials.rs
@@ -428,7 +428,7 @@ unsafe extern "C" fn game_specialhi(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_ON_DROP_BOTH_SIDES);
     }
-    frame(lua_state, 32.0);
+    frame(lua_state, 30.0);
     if is_excute(agent) {
         KineticModule::resume_energy(boma, *FIGHTER_KINETIC_ENERGY_ID_CONTROL);
     }
