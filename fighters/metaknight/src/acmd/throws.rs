a597dc177760aa3f37a9c278a54487fa2bb6addb
SuddyN <suddy121800@hotmail.com>
2025-11-09 20:58:45 -0500

    nerf DThrow more

--- fighters/metaknight/src/acmd/throws.rs
+++ fighters/metaknight/src/acmd/throws.rs
@@ -198,19 +198,19 @@ unsafe extern "C" fn game_throwlw(agent: &mut L2CAgentBase) {
         }
         wait(lua_state, 3.0);
     }
-    frame(lua_state, 73.0);
+    frame(lua_state, 72.0);
     if is_excute(agent) {
     	ATTACK(agent, 0, 0, Hash40::new("top"), 1.0, 32, 80, 0, 70, 7.0, 0.0, 4.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
         AttackModule::set_catch_only_all(boma, true, false);
         CHECK_FINISH_CAMERA(agent, 2, 0);
     }
-    frame(lua_state, 75.0);
-    FT_MOTION_RATE_RANGE(agent, 75.0, 82.0, 12.0);
+    frame(lua_state, 73.0);
+    FT_MOTION_RATE_RANGE(agent, 73.0, 80.0, 14.0);
     if is_excute(agent) {
     	ATK_HIT_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, Hash40::new("throw"), WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT), WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP), WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_NO));
         AttackModule::clear_all(boma);
     }
-    frame(lua_state, 82.0);
+    frame(lua_state, 80.0);
     FT_MOTION_RATE(agent, 1.0);
 }
 
