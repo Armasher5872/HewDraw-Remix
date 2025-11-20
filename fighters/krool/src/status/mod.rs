bb21bb0799a35cb97364d3ca158284ffea2b2cdb
Askew <55216313+SkewedAskew@users.noreply.github.com>
2025-11-20 15:45:06 -0500

    Crown catch ledgegrab

--- fighters/krool/src/status/mod.rs
+++ fighters/krool/src/status/mod.rs
@@ -6,6 +6,7 @@ mod attack_lw4;
 mod special_hi;
 mod special_lw;
 mod special_n;
+mod special_s;
 
 // handle damage to belly
 #[no_mangle]
@@ -64,4 +65,5 @@ pub fn install(agent: &mut Agent) {
     special_hi::install(agent);
     special_lw::install(agent);
     special_n::install(agent);
+    special_s::install(agent);
 }
