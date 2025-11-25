7320f07b771a424819b04f3152be8c8fac61fd5f
Askew <55216313+SkewedAskew@users.noreply.github.com>
2025-11-25 18:45:15 -0500

    Update special_lw.rs

--- fighters/miigunner/src/acmd/specials/special_lw.rs
+++ fighters/miigunner/src/acmd/specials/special_lw.rs
@@ -134,7 +134,8 @@ unsafe extern "C" fn game_speciallw3end(agent: &mut L2CAgentBase) {
 	frame(lua_state, 1.0);
 	if is_excute(agent) {
 		let offset_y = if agent.is_situation(*SITUATION_KIND_GROUND) { 6.5 } else { 9.5 };
-        ATTACK(agent, 0, 0, Hash40::new("top"), 8.0, 55, 70, 0, 30, 11.5, 0.0, offset_y, 0.0, None, None, None, 0.8, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_NO_ITEM, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
+        let sdi_mul = if agent.is_situation(*SITUATION_KIND_GROUND) { 1.0 } else { 0.0 };
+        ATTACK(agent, 0, 0, Hash40::new("top"), 8.0, 55, 70, 0, 30, 11.5, 0.0, offset_y, 0.0, None, None, None, 0.8, sdi_mul, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_NO_ITEM, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
     }
 	frame(lua_state, 3.0);
 	if is_excute(agent) {
