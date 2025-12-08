522f6fe8e6a8f2ab82516d2fa6c5b580eb0d29e4
Wish325 <Wishes0325@gmail.com>
2025-12-08 16:03:41 -0600

    hitbox resizing, b-turnaround added

--- fighters/trail/src/acmd/specials.rs
+++ fighters/trail/src/acmd/specials.rs
@@ -700,17 +700,18 @@ unsafe extern "C" fn game_speciallwstart(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 21.0);
     FT_MOTION_RATE(agent,0.5);
     if is_excute(agent) {
-        ATTACK(agent, 0, 0, Hash40::new("rot"), 4.0, 75, 130, 100, 0, 5.5, 0.0, 1.0,0.25, None, None,None, 0.6, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_NONE);
+        ATTACK(agent, 0, 0, Hash40::new("rot"), 4.0, 75, 130, 100, 0, 3.5, 0.0, 1.0, 0.25, None, None,None, 0.6, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_NONE);
         
-        ATTACK(agent, 1, 0, Hash40::new("top"), 4.0, 75, 130, 100, 0, 3.2, 0.0, 2.6, -7.2, Some(0.0), Some(2.6), Some(9.2), 0.6, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_NONE);
+        ATTACK(agent, 1, 0, Hash40::new("top"), 4.0, 75, 130, 100, 0, 3.2, 0.0, 2.6, -7.2, Some(0.0), Some(2.6), Some(8.2), 0.6, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_NONE);
     }
     wait(agent.lua_state_agent, 1.0);
     if is_excute(agent) {
+         ATTACK(agent, 0, 0, Hash40::new("rot"), 4.0, 75, 130, 100, 0, 4.5, 0.0, 1.0, 0.25, None, None,None, 0.6, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_NONE);
         AttackModule::clear(agent.module_accessor,1,false);
     }
     frame(agent.lua_state_agent, 21.0);
     FT_MOTION_RATE(agent,0.5);
-    frame(agent.lua_state_agent, 30.0);
+    frame(agent.lua_state_agent, 40.0);
     if is_excute(agent) {
         CancelModule::enable_cancel(agent.module_accessor);
         notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_ALWAYS_BOTH_SIDES);
@@ -795,7 +796,7 @@ unsafe extern "C" fn game_specialairlwstart(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 20.0);
     for f in (20..34).step_by(2) {
         if is_excute(agent) {
-            ATTACK(agent, 0, 0, Hash40::new("top"), 0.8, 105, 20, 0, 20, 10.5, 0.0, 5.0, 0.0, None, None, None, 0.6, 0.6, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_magic"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_MAGIC, *ATTACK_REGION_NONE);
+            ATTACK(agent, 0, 0, Hash40::new("top"), 0.8, 105, 20, 0, 20, 7.0, 0.0, 6.0, 0.0, None, None, None, 0.6, 0.6, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_magic"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_MAGIC, *ATTACK_REGION_NONE);
         }
         wait(agent.lua_state_agent, 1.0);
         if is_excute(agent) {
@@ -875,6 +876,7 @@ unsafe extern "C" fn expression_specialairlwstart(agent: &mut L2CAgentBase) {
 unsafe extern "C" fn game_specialairlwrebound(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 1.0);
     if is_excute(agent) {
+        QUAKE(agent, *CAMERA_QUAKE_KIND_M);
         ATTACK(agent, 0, 0, Hash40::new("top"), 5.5, 38, 82, 0, 54, 4.7, 0.0, 3.6, -9.2, Some(0.0), Some(3.6), Some(13.2), 0.6, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_NONE);
     }
     wait(agent.lua_state_agent, 2.0);
--- fighters/trail/src/status/special_lw.rs
+++ fighters/trail/src/status/special_lw.rs
@@ -26,6 +26,34 @@ pub unsafe extern "C" fn speciallw_main(fighter: &mut smashline::L2CFighterCommo
     return speciallw_ground_main(fighter);
 }
 
+pub unsafe extern "C" fn speciallw_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
+    StatusModule::init_settings(
+        fighter.module_accessor,
+        SituationKind(*SITUATION_KIND_NONE),
+        *FIGHTER_KINETIC_TYPE_UNIQ,
+        *GROUND_CORRECT_KIND_KEEP as u32,
+        GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_NONE),
+        true,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLAG,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_INT,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLOAT,
+        0
+    );
+
+    FighterStatusModuleImpl::set_fighter_status_data(
+        fighter.module_accessor,
+        false,
+        *FIGHTER_TREADED_KIND_NO_REAC,
+        false,
+        false,
+        false,
+        (*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_LW | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK | *FIGHTER_LOG_MASK_FLAG_ACTION_TRIGGER_ON) as u64,
+        (*FIGHTER_STATUS_ATTR_START_TURN as u32), //important b-reverse part
+        (*FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_LW) as u32,
+        0
+    );
+    0.into()
+}
 /*
 GROUND
 */
@@ -363,7 +391,8 @@ pub unsafe extern "C" fn speciallw_landing_end(fighter: &mut L2CFighterCommon) -
 
 pub fn install(agent: &mut smashline::Agent) {
 	agent.status(Main, *FIGHTER_STATUS_KIND_SPECIAL_LW, speciallw_main);
-
+    
+    agent.status(Pre, *FIGHTER_STATUS_KIND_SPECIAL_LW, speciallw_pre);
     agent.status(Pre, statuses::trail::SPECIAL_LW_LANDING, speciallw_landing_pre);
     agent.status(Main, statuses::trail::SPECIAL_LW_LANDING, speciallw_landing_main);
     agent.status(End, statuses::trail::SPECIAL_LW_LANDING, speciallw_landing_end);
