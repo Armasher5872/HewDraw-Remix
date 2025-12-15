b1c949eaa2248557e14f547d3377b32fef2c27b7
shun-one-1 <one.shun@gmail.com>
2025-12-15 15:48:02 -0600

    fixing implementation 2
    still not done

--- fighters/shulk/src/acmd/mod.rs
+++ fighters/shulk/src/acmd/mod.rs
@@ -10,26 +10,20 @@ mod other;
 
 pub unsafe fn shulk_get_trail(agent: &mut L2CAgentBase) {
     let original_trail = "tex_shulk_sword_hdr";
-    if !FIGHTER_SHULK_INSTANCE_WORK_ID_FLAG_SPECIAL_N_ACTIVE {
-        return original_trail;
+    let boma = agent.boma();
+        if !WorkModule::is_flag(boma, *FIGHTER_SHULK_INSTANCE_WORK_ID_FLAG_SPECIAL_N_ACTIVE) {
+        return;
     }
-    let active_art = FIGHTER_SHULK_INSTANCE_WORK_ID_INT_SPECIAL_N_TYPE;
+    let active_art = WorkModule::get_int(boma, *FIGHTER_SHULK_INSTANCE_WORK_ID_INT_SPECIAL_N_TYPE);
     let trail = match active_art {
         0 => format!("{}{}", original_trail, "_jump"),
         1 => format!("{}{}", original_trail, "_speed"),
         2 => format!("{}{}", original_trail, "_shield"),
         3 => format!("{}{}", original_trail, "_buster"),
         4 => format!("{}{}", original_trail, "_smash"),
-        _ => original_trail
+        _ => original_trail.to_string()
     };
-    AFTER_IMAGE_ON_arg29(agent, Hash40::new(trail), ..);
-}
-
-shulk_get_trail(agent) {
-    frame(lua_state, etc);
-    if is_excute(agent) {
-        shulk_get_trail(agent);
-}
+    AFTER_IMAGE4_ON_arg29(agent, trail.to_hash(), Hash40::new("tex_shulk_sword2"), 8, Hash40::new("haver"), 0, 3, 0.9, Hash40::new("haver"), 0, 19, 1.1, true, Hash40::new("null"), Hash40::new("haver"), 0, 0, 0, 0, 0, 0, 1, 0, *EFFECT_AXIS_X, 0, *TRAIL_BLEND_ALPHA, 101, *TRAIL_CULL_NONE, 1.4, 0.2);
 }
 
 pub fn install(agent: &mut Agent) {
