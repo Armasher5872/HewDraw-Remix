c9adb2b98179e6ada7ff4afd0ca255465f901ab8
Askew <55216313+SkewedAskew@users.noreply.github.com>
2025-12-19 14:14:46 -0500

    Update aerials.rs

--- fighters/miiswordsman/src/acmd/aerials.rs
+++ fighters/miiswordsman/src/acmd/aerials.rs
@@ -47,7 +47,7 @@ unsafe extern "C" fn game_attackairf(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
 	frame(lua_state, 1.0);
-	FT_MOTION_RATE(agent, 0.77); // 10F startup
+	FT_MOTION_RATE_RANGE(agent, 1.0, 10.0, 8.0);
 	if is_excute(agent) {
 		WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
 	}
