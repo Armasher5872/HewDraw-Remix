28e80d6d621490595d75b49ed414f3ca95b48a8a
SirTanknSpank <115139386+SirTanknSpank@users.noreply.github.com>
2025-12-27 05:37:31 -0600

    Oops

--- fighters/dedede/src/gordo/acmd.rs
+++ fighters/dedede/src/gordo/acmd.rs
@@ -171,11 +171,9 @@ unsafe extern "C" fn game_specialsstart(agent: &mut L2CAgentBase){
     let boma = agent.boma();
     let owner_module_accessor = &mut *sv_battle_object::module_accessor((WorkModule::get_int(boma, *WEAPON_INSTANCE_WORK_ID_INT_LINK_OWNER)) as u32);
     if is_excute(agent){
+        WHOLE_HIT(agent, *HIT_STATUS_OFF);
         if WorkModule::is_flag(owner_module_accessor, *FIGHTER_DEDEDE_INSTANCE_WORK_ID_FLAG_GORDO_GET){
             WorkModule::on_flag(boma, *WEAPON_DEDEDE_GORDO_STATUS_WORK_FLAG_VISIBILITY_ON);
-        }
-        else{
-            WHOLE_HIT(agent, *HIT_STATUS_OFF);
         }
     }
     frame(lua_state, 7.0);
@@ -190,11 +188,9 @@ unsafe extern "C" fn game_specialairsstart(agent: &mut L2CAgentBase) {
     let owner_module_accessor = &mut *sv_battle_object::module_accessor((WorkModule::get_int(boma, *WEAPON_INSTANCE_WORK_ID_INT_LINK_OWNER)) as u32);
 
     if is_excute(agent){
+        WHOLE_HIT(agent, *HIT_STATUS_OFF);
         if WorkModule::is_flag(owner_module_accessor, *FIGHTER_DEDEDE_INSTANCE_WORK_ID_FLAG_GORDO_GET){
             WorkModule::on_flag(boma, *WEAPON_DEDEDE_GORDO_STATUS_WORK_FLAG_VISIBILITY_ON);
-        }
-        else{
-            WHOLE_HIT(agent, *HIT_STATUS_OFF);
         }
     }
     frame(lua_state, 7.0);
