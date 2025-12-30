683e7a975d82e96c8e77c1c91586610911ee7611
Brian Allred <brian@allred.io>
2025-12-29 18:12:30 -0600

    Fix crash

--- fighters/common/src/function_hooks/energy/damage.rs
+++ fighters/common/src/function_hooks/energy/damage.rs
@@ -27,7 +27,8 @@ unsafe fn hitstun_fall_speed_1(ctx: &mut skyline::hooks::InlineCtx) {
 
 #[skyline::hook(offset = 0x6c399c, inline)]
 unsafe fn hitstun_gravity_2(ctx: &mut skyline::hooks::InlineCtx) {
-    let boma = ctx.registers[1].x() as *mut smash::app::BattleObjectModuleAccessor;
+    let work_module = ctx.registers[23].x();
+    let boma = *(work_module as *mut *mut BattleObjectModuleAccessor).add(1);
     let air_accel_y = WorkModule::get_param_float(boma, hash40("air_accel_y"), 0);
     let hitstun_gravity_min = ParamModule::get_float((*boma).object(), ParamType::Common, "hitstun_gravity_min");
     let hitstun_gravity_max = ParamModule::get_float((*boma).object(), ParamType::Common, "hitstun_gravity_max");
@@ -39,7 +40,8 @@ unsafe fn hitstun_gravity_2(ctx: &mut skyline::hooks::InlineCtx) {
 
 #[skyline::hook(offset = 0x6c39c4, inline)]
 unsafe fn hitstun_fall_speed_2(ctx: &mut skyline::hooks::InlineCtx) {
-    let boma = ctx.registers[1].x() as *mut smash::app::BattleObjectModuleAccessor;
+    let work_module = ctx.registers[23].x();
+    let boma = *(work_module as *mut *mut BattleObjectModuleAccessor).add(1);
     let air_speed_y_stable = WorkModule::get_param_float(boma, hash40("air_speed_y_stable"), 0);
 
     ctx.registers_f[0].set_s(air_speed_y_stable)
