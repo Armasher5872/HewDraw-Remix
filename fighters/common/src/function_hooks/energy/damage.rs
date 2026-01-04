aad08c0c16d0d336853cb2fe3ee819d7b79d4fe8
Brian Allred <brian@allred.io>
2026-01-03 23:01:33 -0600

    Make trajectory lines stop at end of hitstun

--- fighters/common/src/function_hooks/energy/damage.rs
+++ fighters/common/src/function_hooks/energy/damage.rs
@@ -82,7 +82,8 @@ unsafe fn trajectory_manager_hook(
     let ptr_2 = *(ptr_1.add(0x80) as *const *const u8);
     let show_trajectory_flag = *ptr_2.add(0xb49);
     if show_trajectory_flag == 0 {
-        return call_original!(main_obj, unused_arg, boma_ptr, knockback_info);
+        *(main_obj.add(0x2a9) as *mut u8) = 1;
+        return;
     }
 
     // Check if hit is valid for this
@@ -90,8 +91,11 @@ unsafe fn trajectory_manager_hook(
     let is_valid_hit = *kb_bytes.add(0x111);
     let flag_b = *kb_bytes.add(0x44); // Unsure what this flag is, but it's checked as well
     if is_valid_hit != 0 || flag_b != 0 {
-        return call_original!(main_obj, unused_arg, boma_ptr, knockback_info);
+        *(main_obj.add(0x2a9) as *mut u8) = 1;
+        return;
     }
+
+    *(main_obj.add(0x2a9) as *mut u8) = 0;
 
     // calculate three knockback vectors: normal, min DI, max DI
     let boma = boma_ptr as *mut BattleObjectModuleAccessor;
@@ -117,8 +121,62 @@ unsafe fn trajectory_manager_hook(
     *knockback_info.add(blue_line) = launch_speed_min.x;
     *knockback_info.add(blue_line + 1) = launch_speed_min.y;
 
-    return call_original!(main_obj, unused_arg, boma_ptr, knockback_info);
+    let hitstun = *knockback_info.add(0x48 / 4) as u64;
+
+    let mut buffer_0: [u64; 2] = [0; 2];
+    buffer_0[0] = *(main_obj.add(0x2b0) as *mut u64);
+    buffer_0[1] = 0;
+    let result_0 = calculate_knockback(0, boma_ptr, knockback_info, buffer_0.as_mut_ptr(), hitstun);
+    set_buffer(buffer_0[0] as *mut u8, hitstun);
+
+    let bit_0 = (result_0 & 1) as u8;
+    *(main_obj.add(0x2aa) as *mut u8) |= bit_0;
+    *(main_obj.add(0x2a9) as *mut u8) |= bit_0;
+
+    let mut buffer_1: [u64; 2] = [0; 2];
+    buffer_1[0] = *(main_obj.add(0x2c8) as *mut u64);
+    buffer_1[1] = 0;
+    let result_1 = calculate_knockback(1, boma_ptr, knockback_info, buffer_1.as_mut_ptr(), hitstun);
+    set_buffer(buffer_1[0] as *mut u8, hitstun);
+
+    let bit_1 = (result_1 & 1) as u8;
+    *(main_obj.add(0x2aa) as *mut u8) |= bit_1;
+    *(main_obj.add(0x2a9) as *mut u8) |= bit_1;
+
+    let mut buffer_2: [u64; 2] = [0; 2];
+    buffer_2[0] = *(main_obj.add(0x2e0) as *mut u64);
+    buffer_2[1] = 0;
+    let result_2 = calculate_knockback(2, boma_ptr, knockback_info, buffer_2.as_mut_ptr(), hitstun);
+    set_buffer(buffer_2[0] as *mut u8, hitstun);
+
+    let bit_2 = (result_2 & 1) as u8;
+    *(main_obj.add(0x2aa) as *mut u8) |= bit_2;
+    *(main_obj.add(0x2a9) as *mut u8) |= bit_2;
 }
+
+unsafe fn set_buffer(ptr: *mut u8, duration_frames: u64) {
+    if duration_frames == 0 {
+        return;
+    }
+
+    let last_vector_ptr = (ptr as *mut u8).add((duration_frames - 1) as usize * 16) as *mut f32;
+    let last_vector = Vector2f::new(*last_vector_ptr, *last_vector_ptr.add(1));
+
+    for i in duration_frames - 1 .. 300 {
+        let current_vector = ptr.add(i as usize * 16) as *mut f32;
+
+        *current_vector = last_vector.x;
+        *current_vector.add(1) = last_vector.y;
+    }
+}
+
+#[skyline::from_offset(0x6c33f0)]
+unsafe fn calculate_knockback(
+    line_index: u64, 
+    boma_ptr: *mut u8, 
+    knockback_info: *mut f32, 
+    knockback_values: *mut u64, // maybe?
+    duration_frames: u64) -> u32;
 
 pub fn install() {
     unsafe {
