987f8be75114e2b559b8a87a9785be039dd00901
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2026-02-04 16:55:53 -0500

    rework airdodge ledgegrab frames as offset from cancel_frame

--- fighters/inkling/src/acmd/other.rs
+++ fighters/inkling/src/acmd/other.rs
@@ -52,6 +52,8 @@ unsafe extern "C" fn game_escapeair(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     let escape_air_cancel_frame = WorkModule::get_param_float(boma, hash40("param_motion"), hash40("escape_air_cancel_frame"));
+    let offset = ParamModule::get_float(boma.object(), ParamType::Common, "escape_air_cliff_catch_frame_offset_from_cancel_frame");
+    let ledgegrab_frame = escape_air_cancel_frame + offset;
     if is_excute(agent) {
         WorkModule::on_flag(boma, *FIGHTER_INKLING_INSTANCE_WORK_ID_FLAG_NO_FLIP_SQUID);
     }
@@ -94,7 +96,7 @@ unsafe extern "C" fn game_escapeair(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         ArticleModule::set_visibility_whole(boma, *FIGHTER_INKLING_GENERATE_ARTICLE_SQUID, false, app::ArticleOperationTarget(*ARTICLE_OPE_TARGET_ALL));
     }
-    frame(lua_state, escape_air_cancel_frame);
+    frame(lua_state, ledgegrab_frame);
     if is_excute(agent) {
         notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_ALWAYS_BOTH_SIDES);
     }
@@ -103,6 +105,9 @@ unsafe extern "C" fn game_escapeair(agent: &mut L2CAgentBase) {
 unsafe extern "C" fn game_escapeairslide(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
+    let escape_air_slide_cancel_frame = WorkModule::get_param_float(boma, hash40("param_motion"), hash40("escape_air_slide_cancel_frame"));
+    let offset = ParamModule::get_float(boma.object(), ParamType::Common, "escape_air_cliff_catch_frame_offset_from_cancel_frame");
+    let ledgegrab_frame = escape_air_slide_cancel_frame + offset;
     if is_excute(agent) {
         WorkModule::on_flag(boma, *FIGHTER_INKLING_INSTANCE_WORK_ID_FLAG_NO_FLIP_SQUID);
     }
@@ -141,7 +146,7 @@ unsafe extern "C" fn game_escapeairslide(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         VisibilityModule::set_whole(boma, true);
     }
-    frame(lua_state, 39.0);
+    frame(lua_state, ledgegrab_frame);
     if is_excute(agent) {
         notify_event_msc_cmd!(agent, Hash40::new_raw(0x2127e37c07), *GROUND_CLIFF_CHECK_KIND_ALWAYS_BOTH_SIDES);
     }
