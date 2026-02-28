62eb00aa6c4c5b72bf2dabd4187aaa3a36db654e
SuddyN <suddy121800@hotmail.com>
2026-02-28 14:10:58 -0500

    arrow second draft

--- fighters/pitb/src/bowarrow/opff.rs
+++ fighters/pitb/src/bowarrow/opff.rs
@@ -3,22 +3,22 @@ utils::import_noreturn!(common::opff::fighter_common_opff);
 use super::*;
 use globals::*;
 
-pub unsafe extern "C" fn pitb_bowarrow_frame(weapon: &mut smash::lua2cpp::L2CFighterBase) {
-    let boma = weapon.boma();
-    if StatusModule::status_kind(boma) == *WEAPON_PIT_BOWARROW_STATUS_KIND_FLY
-    && AttackModule::is_infliction(boma, *COLLISION_KIND_MASK_PARRY) {
-        EffectModule::req_follow(weapon.module_accessor, Hash40::new("sys_status_defense_up"), Hash40::new("hip"), &Vector3f::new(0.7, 0.0, 0.0), &Vector3f::zero(), 0.7, true, 0, 0, 0, 0, 0, true, true) as u32;
-        let magnitude = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
-        let angle = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
-        let snapped_angle = (angle / std::f32::consts::FRAC_PI_4).round() * std::f32::consts::FRAC_PI_4;
-        let new_speed_x = magnitude * snapped_angle.cos();
-        let new_speed_y = magnitude * snapped_angle.sin();
-        sv_kinetic_energy!(set_speed, weapon, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, new_speed_x, new_speed_y);
-        weapon.set_float(magnitude, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
-        weapon.set_float(snapped_angle, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
-    }
-}
+// pub unsafe extern "C" fn pitb_bowarrow_frame(weapon: &mut smash::lua2cpp::L2CFighterBase) {
+//     let boma = weapon.boma();
+//     if StatusModule::status_kind(boma) == *WEAPON_PIT_BOWARROW_STATUS_KIND_FLY
+//     && AttackModule::is_infliction(boma, *COLLISION_KIND_MASK_PARRY) {
+//         EffectModule::req_follow(weapon.module_accessor, Hash40::new("sys_status_defense_up"), Hash40::new("hip"), &Vector3f::new(0.7, 0.0, 0.0), &Vector3f::zero(), 0.7, true, 0, 0, 0, 0, 0, true, true) as u32;
+//         let magnitude = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
+//         let angle = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
+//         let snapped_angle = (angle / std::f32::consts::FRAC_PI_4).round() * std::f32::consts::FRAC_PI_4;
+//         let new_speed_x = magnitude * snapped_angle.cos();
+//         let new_speed_y = magnitude * snapped_angle.sin();
+//         sv_kinetic_energy!(set_speed, weapon, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, new_speed_x, new_speed_y);
+//         weapon.set_float(magnitude, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
+//         weapon.set_float(snapped_angle, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
+//     }
+// }
 
 pub fn install(agent: &mut Agent) {
-    agent.on_line(Main, pitb_bowarrow_frame);
+    // agent.on_line(Main, pitb_bowarrow_frame);
 }
--- fighters/pitb/src/bowarrow/status.rs
+++ fighters/pitb/src/bowarrow/status.rs
@@ -1,25 +1,46 @@
 use super::*;
 
-unsafe extern "C" fn fly_check_attack(weapon: &mut L2CWeaponCommon, param_2: &L2CValue, param_3: &L2CValue) -> L2CValue {
-    println!("running");
-    // force knockdown
-    if (&param_3["kind_"]).get_i32() == *COLLISION_KIND_SHIELD
-    // && AttackModule::is_infliction_status(weapon.module_accessor, *COLLISION_KIND_MASK_PARRY) 
-    {
-        EffectModule::req_follow(weapon.module_accessor, Hash40::new("sys_status_defense_up"), Hash40::new("hip"), &Vector3f::new(0.7, 0.0, 0.0), &Vector3f::zero(), 0.7, true, 0, 0, 0, 0, 0, true, true) as u32;
-        println!("hit a shield");
-        let speed_x = weapon.get_speed_x(*WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL);
-        let speed_y = weapon.get_speed_y(*WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL);
-        let magnitude = speed_x.hypot(speed_y);
-        let angle = speed_y.atan2(speed_x);
-        let snapped_angle = (angle / (std::f32::consts::PI / 4.0)).round() * (std::f32::consts::PI / 4.0);
-        let new_speed_x = magnitude * snapped_angle.cos();
-        let new_speed_y = magnitude * snapped_angle.sin();
-        sv_kinetic_energy!(set_speed, weapon, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, new_speed_x, new_speed_y);
+unsafe extern "C" fn fly_exec(weapon: &mut L2CWeaponCommon) -> L2CValue {
+    if weapon.is_flag(*WEAPON_INSTANCE_WORK_ID_FLAG_SWALLOWED) {
+        return false.into();
     }
+
+    let stick_x = weapon.stick_x();
+    let stick_y = weapon.stick_y();
+    let magnitude = (stick_x.powi(2) + stick_y.powi(2)).sqrt();
+    let angle = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
+    let mut new_angle = angle;
+
+    // when parried, snap the arrow to one of 8 directions
+    if AttackModule::is_infliction_status(weapon.module_accessor, *COLLISION_KIND_MASK_REFLECTOR | *COLLISION_KIND_MASK_PARRY) {
+        new_angle = (angle / 45.0).round() * 45.0;
+        AttackModule::clear_inflict_kind_status(weapon.module_accessor);
+    }
+    // else, control the arrow direction with the stick
+    else if magnitude > 1e-5 {
+        let stick_angle_deg = stick_y.atan2(stick_x).to_degrees();
+        let mut delta = angle - stick_angle_deg;
+
+        while delta > 180.0  { delta -= 360.0; }
+        while delta < -180.0 { delta += 360.0; }
+
+        let control_angle = weapon.get_param_float("param_bowarrow", "control_angle");
+        let max_turn = control_angle * magnitude;
+        let clamped_delta = delta.clamp(-max_turn, max_turn);
+        new_angle = angle - clamped_delta;
+    }
+
+    let speed = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
+    let speed_x = speed * new_angle.to_radians().cos();
+    let speed_y = speed * new_angle.to_radians().sin();
+    sv_kinetic_energy!(set_speed, weapon, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, speed_x, speed_y);
+    weapon.set_float(speed, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
+    weapon.set_float(new_angle, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
+    PostureModule::set_rot(weapon.module_accessor, &Vector3f::new(-new_angle, 90.0, 0.0), 0);
+
     return false.into();
 }
 
 pub fn install(agent: &mut Agent) {
-    agent.status(CheckAttack, *WEAPON_PIT_BOWARROW_STATUS_KIND_FLY, fly_check_attack);
+    agent.status(Exec, *WEAPON_PIT_BOWARROW_STATUS_KIND_FLY, fly_exec);
 }
