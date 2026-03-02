115491e885c17d6492baded82121c0f1182069bd
SuddyN <suddy121800@hotmail.com>
2026-03-01 20:01:08 -0500

    link boomerang fix

--- fighters/link/src/boomerang/opff.rs
+++ fighters/link/src/boomerang/opff.rs
@@ -3,7 +3,7 @@ utils::import_noreturn!(common::opff::fighter_common_opff);
 use super::*;
 use globals::*;
 
-unsafe fn krown_reflect_fix(weapon: &mut smash::lua2cpp::L2CFighterBase) {
+unsafe fn boomerang_reflect_fix(weapon: &mut smash::lua2cpp::L2CFighterBase) {
     // this opff runs once after reflection
     let boma = weapon.module_accessor;
     if !AttackModule::is_infliction_status(boma, *COLLISION_KIND_MASK_REFLECTOR | *COLLISION_KIND_MASK_PARRY) {
@@ -11,7 +11,7 @@ unsafe fn krown_reflect_fix(weapon: &mut smash::lua2cpp::L2CFighterBase) {
     }
     AttackModule::clear_inflict_kind_status(boma);
 
-    // update position, because it uses an animation that moves the rot bone but not top
+    // update position, because krown uses an animation that moves the rot bone but not top
     let top_pos = *PostureModule::pos(boma);
     let rot_pos = &mut Vector3f{ x: 0.0, y: 0.0, z: 0.0 };
     ModelModule::joint_global_position(boma, Hash40::new("rot"), rot_pos, false);
@@ -20,19 +20,22 @@ unsafe fn krown_reflect_fix(weapon: &mut smash::lua2cpp::L2CFighterBase) {
     // update facing direction depending on whether it is traveling forwards or backwards
     let lr = PostureModule::lr(boma);
     let speed_x = KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_ALL);
-    if weapon.is_status(*WEAPON_KROOL_CROWN_STATUS_KIND_THROW) && weapon.motion_frame() <= 40.0 { // F40 is the frame that animation reverses direction
+    if weapon.is_status(*WN_LINK_BOOMERANG_STATUS_KIND_FLY) {
         PostureModule::reverse_lr(boma);
         PostureModule::update_rot_y_lr(boma);
     }
 
+    // lifetime fix
+    weapon.set_int_from_param(*WN_LINK_BOOMERANG_INSTANCE_WORK_ID_INT_LIFE, "boomerang", "life");
+
     // restart status
-    weapon.change_status(WEAPON_KROOL_CROWN_STATUS_KIND_THROW.into(), false.into());
+    weapon.change_status(WN_LINK_BOOMERANG_STATUS_KIND_FLY.into(), false.into());
 }
 
-pub unsafe extern "C" fn krool_crown_frame(weapon: &mut smash::lua2cpp::L2CFighterBase) {
-    krown_reflect_fix(weapon);
+pub unsafe extern "C" fn link_boomerang_frame(weapon: &mut smash::lua2cpp::L2CFighterBase) {
+    boomerang_reflect_fix(weapon);
 }
 
 pub fn install(agent: &mut Agent) {
-    agent.on_line(Main, krool_crown_frame);
+    agent.on_line(Main, link_boomerang_frame);
 }
