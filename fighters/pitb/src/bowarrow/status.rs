7f903ed51530de0d033cc76ccba4c3d8858033a6
SuddyN <suddy121800@hotmail.com>
2026-03-02 19:13:35 -0500

    arrows snap direction instead of being controlled by reflectee

--- fighters/pitb/src/bowarrow/status.rs
+++ fighters/pitb/src/bowarrow/status.rs
@@ -5,37 +5,34 @@ unsafe extern "C" fn fly_exec(weapon: &mut L2CWeaponCommon) -> L2CValue {
         return false.into();
     }
 
-    // use get_team_owner_boma to allow the arrow to be controlled by opponent after reflecting it
-    let weapon_boma = weapon.module_accessor;
-    let owner_boma = weapon.get_team_owner_boma();
-    let (stick_mag, stick_rad) = owner_boma.stick_polar();
-
-    // adjust arrow angle according to stick
+    let (stick_mag, stick_rad) = weapon.stick_polar();
     let arrow_deg = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
-    let arrow_delta = if stick_mag > 1e-5 {
+    let was_reflected = AttackModule::is_infliction_status(weapon.module_accessor, *COLLISION_KIND_MASK_REFLECTOR | *COLLISION_KIND_MASK_PARRY);
+
+    let new_deg = if was_reflected { // when parried, snap the arrow to one of 8 directions
+        AttackModule::clear_inflict_kind_status(weapon.module_accessor);
+        (arrow_deg / 45.0).round() * 45.0
+    }
+    else if stick_mag > 1e-5 { // if stick is not neutral, control the arrow
         let mut delta = arrow_deg - stick_rad.to_degrees();
         while delta > 180.0  { delta -= 360.0; }
         while delta < -180.0 { delta += 360.0; }
 
-        let was_reflected = AttackModule::is_infliction_status(weapon_boma, *COLLISION_KIND_MASK_REFLECTOR | *COLLISION_KIND_MASK_PARRY);
-        let control_angle_mul = if was_reflected { 2.0 } else { 1.0 }; // when reflected, new owner has increased control over angle
-        let control_angle = weapon.get_param_float("param_bowarrow", "control_angle") * control_angle_mul;
+        let control_angle = weapon.get_param_float("param_bowarrow", "control_angle");
         let max_turn = control_angle * stick_mag;
-
-        delta.clamp(-max_turn, max_turn)
+        arrow_deg - delta.clamp(-max_turn, max_turn)
     } else {
-        0.0
+        arrow_deg
     };
 
-    // apply new velocity vector
-    let new_deg = arrow_deg - arrow_delta;
+    // apply new angle to velocity
     let speed = weapon.get_float(*WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
     let speed_x = speed * new_deg.to_radians().cos();
     let speed_y = speed * new_deg.to_radians().sin();
     sv_kinetic_energy!(set_speed, weapon, WEAPON_KINETIC_ENERGY_RESERVE_ID_NORMAL, speed_x, speed_y);
     weapon.set_float(speed, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_SPEED);
     weapon.set_float(new_deg, *WEAPON_PIT_BOWARROW_INSTANCE_WORK_ID_FLOAT_ANGLE);
-    PostureModule::set_rot(weapon_boma, &Vector3f::new(-new_deg, 90.0, 0.0), 0);
+    PostureModule::set_rot(weapon.module_accessor, &Vector3f::new(-new_deg, 90.0, 0.0), 0);
 
     return false.into();
 }
