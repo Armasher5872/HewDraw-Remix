efb1a6bd8f7cb222e7729af3969bbeb5f11da6e9
SuddyN <suddy121800@hotmail.com>
2026-03-02 20:20:49 -0500

    normals nerfs

--- fighters/marth/src/acmd/ground.rs
+++ fighters/marth/src/acmd/ground.rs
@@ -3,6 +3,10 @@ use super::*;
 unsafe extern "C" fn game_attack11(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
+    frame(lua_state, 1.0);
+    FT_MOTION_RATE_RANGE(agent, 1.0, 2.0, 3.0);
+    frame(lua_state, 2.0);
+    FT_MOTION_RATE(agent, 1.0);
     frame(lua_state, 4.0);
     if is_excute(agent) {
         ATTACK(agent, 0, 0, Hash40::new("sword2"), 3.0, 78, 30, 0, 55,      4.0, 0.0, 0.0, 0.6, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
