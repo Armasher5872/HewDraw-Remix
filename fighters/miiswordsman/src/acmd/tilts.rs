3e30ab5fda88664c47f0d593c795523bb6945b6c
Askew <55216313+SkewedAskew@users.noreply.github.com>
2026-03-06 15:14:47 -0500

    Update tilts.rs

--- fighters/miiswordsman/src/acmd/tilts.rs
+++ fighters/miiswordsman/src/acmd/tilts.rs
@@ -65,7 +65,7 @@ unsafe extern "C" fn game_attacklw3(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
 	frame(lua_state, 5.0);
-	FT_MOTION_RATE_RANGE(agent, 5.0, 7.0, 3.0);
+	FT_MOTION_RATE_RANGE(agent, 5.0, 7.0, 4.0);
 	if is_excute(agent) {
 		ATTACK(agent, 0, 0, Hash40::new("armr"), 8.0, 30, 40, 0, 25, 3.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
 		ATTACK(agent, 1, 0, Hash40::new("haver"), 8.0, 30, 40, 0, 25, 3.0, 0.0, 2.5, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
