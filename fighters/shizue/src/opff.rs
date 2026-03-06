6dc3a6027c110337e818fccd3d1286951121f848
Askew <55216313+SkewedAskew@users.noreply.github.com>
2026-03-06 15:02:47 -0500

    Faulty equipment

--- fighters/shizue/src/opff.rs
+++ fighters/shizue/src/opff.rs
@@ -40,13 +40,13 @@ unsafe fn reel_in(boma: &mut BattleObjectModuleAccessor) {
     }
 
     // disable the opponent's collision with platforms when reeling them in
-    if boma.is_status(*FIGHTER_SHIZUE_STATUS_KIND_SPECIAL_S_HIT) {
-        let caught_id = boma.get_int(*FIGHTER_SHIZUE_STATUS_WORK_ID_SPECIAL_S_INT_TARGET_OBJECT_ID);
-        if sv_battle_object::category(caught_id as u32) == *BATTLE_OBJECT_CATEGORY_FIGHTER {
-            let caught_boma = sv_battle_object::module_accessor(caught_id as u32);
-            GroundModule::set_passable_check(caught_boma, true);
-        }
-    }
+    // if boma.is_status(*FIGHTER_SHIZUE_STATUS_KIND_SPECIAL_S_HIT) {
+    //     let caught_id = boma.get_int(*FIGHTER_SHIZUE_STATUS_WORK_ID_SPECIAL_S_INT_TARGET_OBJECT_ID);
+    //     if sv_battle_object::category(caught_id as u32) == *BATTLE_OBJECT_CATEGORY_FIGHTER {
+    //         let caught_boma = sv_battle_object::module_accessor(caught_id as u32);
+    //         GroundModule::set_passable_check(caught_boma, true);
+    //     }
+    // }
 }
 
 //Lloid Trap Fire Jump Cancel
--- fighters/shizue/src/status/special_s.rs
+++ fighters/shizue/src/status/special_s.rs
@@ -231,6 +231,18 @@ unsafe extern "C" fn special_s_reel_main_loop(fighter: &mut L2CFighterCommon) ->
 
 // FIGHTER_SHIZUE_STATUS_KIND_SPECIAL_S_HIT
 
+unsafe extern "C" fn special_s_hit_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    let ret = smashline::original_status(Main, fighter, *FIGHTER_SHIZUE_STATUS_KIND_SPECIAL_S_HIT)(fighter);
+
+    let caught_id = fighter.get_int(*FIGHTER_SHIZUE_STATUS_WORK_ID_SPECIAL_S_INT_TARGET_OBJECT_ID);
+    if sv_battle_object::category(caught_id as u32) == *BATTLE_OBJECT_CATEGORY_FIGHTER {
+        let caught_boma = sv_battle_object::module_accessor(caught_id as u32);
+        GroundModule::set_passable_check(caught_boma, true);
+    }
+
+    return ret;
+}
+
 unsafe extern "C" fn special_s_hit_end(fighter: &mut L2CFighterCommon) -> L2CValue {
     let caught_id = fighter.get_int(*FIGHTER_SHIZUE_STATUS_WORK_ID_SPECIAL_S_INT_TARGET_OBJECT_ID);
 
@@ -245,5 +257,7 @@ unsafe extern "C" fn special_s_hit_end(fighter: &mut L2CFighterCommon) -> L2CVal
 pub fn install(agent: &mut Agent) {
     agent.status(Pre, *FIGHTER_STATUS_KIND_SPECIAL_S, special_s_pre);
     agent.status(Main, *FIGHTER_SHIZUE_STATUS_KIND_SPECIAL_S_REEL, special_s_reel_main);
+
+    agent.status(Main, *FIGHTER_SHIZUE_STATUS_KIND_SPECIAL_S_HIT, special_s_hit_main);
     agent.status(End, *FIGHTER_SHIZUE_STATUS_KIND_SPECIAL_S_HIT, special_s_hit_end);
 }
