2624ba5f68bb7fc96976d74006a14c432efd5c55
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2026-03-20 19:16:05 -0400

    fix buddy/murabito wavelands after upB

--- fighters/buddy/src/status/landing.rs
+++ fighters/buddy/src/status/landing.rs
@@ -7,6 +7,7 @@ unsafe extern "C" fn landing_light_pre(fighter: &mut L2CFighterCommon) -> L2CVal
     || fighter.global_table[PREV_STATUS_KIND] == FIGHTER_BUDDY_STATUS_KIND_SPECIAL_HI_JUMP {
         let lag = WorkModule::get_param_int(fighter.module_accessor, hash40("param_special_hi"), hash40("landing_frame"));
         VarModule::set_float(fighter.battle_object, vars::common::instance::LAND_CANCEL_LAG, lag as f32);
+        WorkModule::set_float(fighter.module_accessor, 0.0, *FIGHTER_INSTANCE_WORK_ID_FLOAT_LANDING_FRAME);
         fighter.set_status_kind_interrupt(*FIGHTER_STATUS_KIND_LANDING);
         return 1.into();
     }
@@ -20,6 +21,7 @@ unsafe extern "C" fn landing_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
     || fighter.global_table[PREV_STATUS_KIND] == FIGHTER_BUDDY_STATUS_KIND_SPECIAL_HI_JUMP {
         let lag = WorkModule::get_param_int(fighter.module_accessor, hash40("param_special_hi"), hash40("landing_frame"));
         VarModule::set_float(fighter.battle_object, vars::common::instance::LAND_CANCEL_LAG, lag as f32);
+        WorkModule::set_float(fighter.module_accessor, 0.0, *FIGHTER_INSTANCE_WORK_ID_FLOAT_LANDING_FRAME);
     }
     smashline::original_status(Pre, fighter, *FIGHTER_STATUS_KIND_LANDING)(fighter)
 }
