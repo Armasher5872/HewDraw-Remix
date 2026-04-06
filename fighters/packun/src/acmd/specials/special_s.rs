da5cea4e574f3732f057ae0610f0d0ae62cc9823
Askew <55216313+SkewedAskew@users.noreply.github.com>
2026-04-06 18:23:40 -0400

    Update special_s.rs

--- fighters/packun/src/acmd/specials/special_s.rs
+++ fighters/packun/src/acmd/specials/special_s.rs
@@ -228,7 +228,7 @@ unsafe extern "C" fn effect_specialsshoots(agent: &mut L2CAgentBase) {
             LANDING_EFFECT(agent, Hash40::new("sys_action_smoke_h"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
         }
         if WorkModule::get_int(boma, *FIGHTER_PACKUN_INSTANCE_WORK_ID_INT_SPECIAL_S_COUNT) == 60 {
-            EFFECT_FOLLOW_FLIP(agent, Hash40::new("sys_unblockable_flash"), Hash40::new("sys_unblockable_flash"), Hash40::new("top"), -5, 16, 1, 0, 0, 0, 0.75, true, *EF_FLIP_YZ);
+            EFFECT_FOLLOW_FLIP(agent, Hash40::new("sys_unblockable_flash"), Hash40::new("sys_unblockable_flash"), Hash40::new("top"), 7, 18, -5, 0, 0, 0, 0.8, true, *EF_FLIP_YZ);
         }
     }
     frame(lua_state, 19.0);
