eead2ada13a240c66489f6faf32a16a678994730
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2026-04-06 12:38:30 -0400

    fix parried burst specials

--- fighters/miiswordsman/src/status/special_s3.rs
+++ fighters/miiswordsman/src/status/special_s3.rs
@@ -180,8 +180,7 @@ pub unsafe extern "C" fn special_s3_end_init(fighter: &mut L2CFighterCommon) -> 
 
         // Reduce speed on shield
         let prev_inflict_status = VarModule::get_int(fighter.battle_object, vars::common::instance::PREV_STATUS_INFLICT_STATUS);
-        if prev_inflict_status == *COLLISION_KIND_MASK_SHIELD
-        || prev_inflict_status == *COLLISION_KIND_MASK_PARRY {
+        if prev_inflict_status & *COLLISION_KIND_MASK_SHIELD | *COLLISION_KIND_MASK_PARRY != 0 {
             let shield_hit_end_speed_x = ParamModule::get_float(fighter.battle_object, ParamType::Agent, "param_special_s3.shield_hit_end_speed_x");
             let lr = PostureModule::lr(fighter.module_accessor);
             sv_kinetic_energy!(
@@ -244,8 +243,7 @@ unsafe extern "C" fn special_s3_end_main(fighter: &mut L2CFighterCommon) -> L2CV
 
             // Reduce speed on shield
             let prev_inflict_status = VarModule::get_int(fighter.battle_object, vars::common::instance::PREV_STATUS_INFLICT_STATUS);
-            if prev_inflict_status == *COLLISION_KIND_MASK_SHIELD
-            || prev_inflict_status == *COLLISION_KIND_MASK_PARRY {
+            if prev_inflict_status & *COLLISION_KIND_MASK_SHIELD | *COLLISION_KIND_MASK_PARRY != 0 {
                 let shield_hit_end_speed_x = ParamModule::get_float(fighter.battle_object, ParamType::Agent, "param_special_s3.shield_hit_end_speed_x");
                 let lr = PostureModule::lr(fighter.module_accessor);
                 sv_kinetic_energy!(set_speed, fighter, FIGHTER_KINETIC_ENERGY_ID_STOP, shield_hit_end_speed_x * lr, 0.0);
