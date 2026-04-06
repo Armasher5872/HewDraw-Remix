1f97a2dbef07541d42d8d1c6470eb1c1bab4b425
Askew <55216313+SkewedAskew@users.noreply.github.com>
2026-04-05 20:42:12 -0400

    Fishing Rod once-per-airtime

--- fighters/shizue/src/acmd/specials.rs
+++ fighters/shizue/src/acmd/specials.rs
@@ -107,6 +107,7 @@ unsafe extern "C" fn game_specialairsstart(agent: &mut L2CAgentBase) {
     let boma = agent.boma();
     frame(lua_state, 1.0);
     if is_excute(agent) {
+        VarModule::on_flag(agent.battle_object, vars::common::instance::SIDE_SPECIAL_CANCEL_NO_HIT);
         FT_MOTION_RATE(agent, 13.0/(21.0 - 1.0));
     }
     if is_excute(agent) {
