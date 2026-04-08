339c2d257941e634d815aacb4f2740c648f9dfec
WuBoytH <alexwuief@gmail.com>
2026-04-08 19:10:10 -0400

    Monado Beat Nerfs

--- fighters/common/src/function_hooks/vtables/shulk.rs
+++ fighters/common/src/function_hooks/vtables/shulk.rs
@@ -12,8 +12,44 @@ pub unsafe extern "C" fn shulk_check_valid_arts_statuses(fighter: &mut Fighter) 
     shulk_check_valid_arts_statuses_inner(fighter)
 }
 
+macro_rules! decl_hooks_shulk_change_arts {
+    ($install_fn:ident => $func:expr; $($name:ident($offset:expr));*) => {
+        $(
+            #[skyline::hook(offset = $offset, inline)]
+            unsafe extern "C" fn $name(ctx: &mut skyline::hooks::InlineCtx) {
+                $func(ctx);
+            }
+        )*
+        fn $install_fn() {
+            skyline::install_hooks!(
+                $(
+                    $name,
+                )*
+            );
+        }
+    }
+}
+
+unsafe extern "C" fn disable_arts_on_beat(ctx: &mut skyline::hooks::InlineCtx) {
+    let fighter = ctx.registers[21].x() as *mut Fighter;
+    let object = &mut (*fighter).battle_object;
+    if VarModule::is_flag(object, vars::shulk::status::MONADO_BEAT) {
+        reset_arts(fighter, 1);
+    }
+}
+
+#[skyline::from_offset(0x116a420)]
+unsafe extern "C" fn reset_arts(fighter: *mut Fighter, param_2: u64);
+
+decl_hooks_shulk_change_arts! {
+    install_disable_arts_on_beat_hooks => disable_arts_on_beat;
+    shulk_arts_change_status_1(0x1167bbc);
+    shulk_arts_change_status_2(0x1168d94)
+}
+
 pub fn install() {
     skyline::install_hooks!(
         shulk_check_valid_arts_statuses,
     );
+    install_disable_arts_on_beat_hooks();
 }
--- fighters/shulk/src/vtable_hook.rs
+++ fighters/shulk/src/vtable_hook.rs
@@ -7,7 +7,8 @@ pub unsafe extern "C" fn shulk_check_valid_arts_statuses_inner(fighter: &mut Fig
     let module_accessor = (fighter.battle_object).module_accessor;
     let status = StatusModule::status_kind(module_accessor);
     let object = &mut fighter.battle_object;
-    if VarModule::is_flag(object, vars::shulk::status::MONADO_BEAT) {
+    if WorkModule::is_flag(module_accessor, *FIGHTER_SHULK_INSTANCE_WORK_ID_FLAG_SPECIAL_N_ACTIVE)
+    && VarModule::is_flag(object, vars::shulk::status::MONADO_BEAT) {
         [
             *FIGHTER_STATUS_KIND_WAIT,
             *FIGHTER_STATUS_KIND_WALK,
