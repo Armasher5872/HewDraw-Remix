684a3e05c19acd84794bedcfc4f52665ce424e9e
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2026-04-15 09:54:19 -0400

    Update damageflyreflect.rs

--- fighters/common/src/general_statuses/damageflyreflect.rs
+++ fighters/common/src/general_statuses/damageflyreflect.rs
@@ -20,6 +20,8 @@ fn nro_hook(info: &skyline::nro::NroInfo) {
 pub unsafe fn end_damage_reflect_stop(fighter: &mut L2CFighterCommon) {
     let damage_speed_x = fighter.get_speed_x(*FIGHTER_KINETIC_ENERGY_ID_DAMAGE);
     let damage_speed_y = fighter.get_speed_y(*FIGHTER_KINETIC_ENERGY_ID_DAMAGE);
+    // damage_fly_reflect_reaction_frame_mul is also applied to hitstun on stage spikes,
+    // handled in a separate location
     let damage_fly_reflect_reaction_frame_mul = WorkModule::get_param_float(fighter.module_accessor, hash40("common"), hash40("damage_fly_reflect_reaction_frame_mul"));
     let new_speed = Vector2f::new(
         damage_speed_x * damage_fly_reflect_reaction_frame_mul,
