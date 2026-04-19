a26c28a0838651ae7e6816270771a1864c81bac4
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2026-04-19 15:30:53 -0400

    Update specials.rs

--- fighters/elight/src/acmd/specials.rs
+++ fighters/elight/src/acmd/specials.rs
@@ -1070,6 +1070,7 @@ unsafe extern "C" fn game_specialsend(agent: &mut L2CAgentBase) {
 
 unsafe extern "C" fn effect_specialhistart(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
     frame(lua_state, 7.0);
     if is_excute(agent) {
         EFFECT_FOLLOW(agent, Hash40::new("elight_sword_open"), Hash40::new("sword1"), 0, 0, 0, 0, 90, 0, 1, true);
