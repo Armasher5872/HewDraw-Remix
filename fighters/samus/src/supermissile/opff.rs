e69eb703541ec32eaca3d791e425c1481ada6540
Baskew <55216313+SkewedAskew@users.noreply.github.com>
2026-04-19 17:00:00 -0400

    Update opff.rs

--- fighters/samus/src/supermissile/opff.rs
+++ fighters/samus/src/supermissile/opff.rs
@@ -5,8 +5,6 @@ use globals::*;
 
 unsafe extern "C" fn supermissile_frame(weapon: &mut L2CFighterBase) {
     let h = weapon.get_int(*WEAPON_SAMUS_SUPERMISSILE_STATUS_STRAIGHT_WORK_ID_INT_FRAME);
-    println!("missile life: {}", h);
-    println!();
     if AttackModule::is_infliction(weapon.module_accessor, *COLLISION_KIND_MASK_REFLECTOR | *COLLISION_KIND_MASK_PARRY) {
         let life = weapon.get_param_int("param_supermissile", "s_life");
         weapon.set_int(life, *WEAPON_SAMUS_SUPERMISSILE_STATUS_STRAIGHT_WORK_ID_INT_FRAME);
