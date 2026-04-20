b7a59f0646ccc535086badec32275fce4db8a580
Baskew <55216313+SkewedAskew@users.noreply.github.com>
2026-04-20 11:06:28 -0400

    Update throws.rs

--- fighters/murabito/src/acmd/throws.rs
+++ fighters/murabito/src/acmd/throws.rs
@@ -118,6 +118,10 @@ unsafe extern "C" fn game_throwb(agent: &mut L2CAgentBase) {
         ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 11.0, 135, 102, 0, 46, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
         ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 60, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
     }
+    frame(lua_state, 1.0);
+    FT_MOTION_RATE_RANGE(agent, 1.0, 7.0, 12.0);
+    frame(lua_state, 7.0);
+    FT_MOTION_RATE(agent, 1.0);
     frame(lua_state, 9.0);
     if is_excute(agent) {
         CHECK_FINISH_CAMERA(agent, -2, 0);
