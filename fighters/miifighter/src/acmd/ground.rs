c13298c8d2c834aee57802f43129b875524a5258
Baskew <55216313+SkewedAskew@users.noreply.github.com>
2026-04-30 12:54:50 -0400

    Brawler

--- fighters/miifighter/src/acmd/ground.rs
+++ fighters/miifighter/src/acmd/ground.rs
@@ -165,6 +165,7 @@ unsafe extern "C" fn game_attackdash(agent: &mut L2CAgentBase) {
         ATTACK(agent, 2, 0, Hash40::new("kneel"), 5.0, 49, 125, 0, 20, 3.25, 2.5, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
     }
     frame(lua_state, 18.0);
+    sv_kinetic_energy!(set_speed_mul, agent, FIGHTER_KINETIC_ENERGY_ID_MOTION, 0.9);
     if is_excute(agent) {
         AttackModule::clear_all(boma);
     }
