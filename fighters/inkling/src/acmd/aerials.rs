c323eadb1312eea48a9a140c28bf7bab49ad5a88
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2026-05-04 14:29:45 -0400

    update inkling dair splash hitsfx

--- fighters/inkling/src/acmd/aerials.rs
+++ fighters/inkling/src/acmd/aerials.rs
@@ -205,8 +205,8 @@ unsafe extern "C" fn game_attackairlw(agent: &mut L2CAgentBase) {
         ATTACK(agent, 2, 0, Hash40::new("haver"), 9.0, 65, 100, 0, 10, 4.5, 0.0, 0.5, 3.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_OBJECT);
         AttackModule::set_ink_value(boma, 1, 25.0);
         AttackModule::set_ink_value(boma, 2, 25.0);
-        AttackModule::set_optional_hit_sound(boma, 1, Hash40::new("se_common_water_hit_l"));
-        AttackModule::set_optional_hit_sound(boma, 2, Hash40::new("se_common_water_hit_l"));
+        AttackModule::set_optional_hit_sound(boma, 1, Hash40::new("se_common_water_hit_m"));
+        AttackModule::set_optional_hit_sound(boma, 2, Hash40::new("se_common_water_hit_m"));
     }
     frame(lua_state, 24.0);
     FT_MOTION_RATE(agent, 1.0);
